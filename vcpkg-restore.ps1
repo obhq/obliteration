22dd14f3a228283adbcc23ee992dbab1af889ade
Putta Khunchalee <ultimaweapon@outlook.com>
2022-09-15 10:49:10 +0700

    Changes VCPKG triplet on Windows to x64-windows-static-md

--- vcpkg-restore.ps1
+++ vcpkg-restore.ps1
@@ -1,5 +1,5 @@
 $packages = Get-Content vcpkg-packages.txt
 
 foreach ($package in $packages) {
-  .\vcpkg\vcpkg.exe --triplet=x64-windows-static --vcpkg-root=vcpkg install $package
+  .\vcpkg\vcpkg.exe --triplet=x64-windows-static-md --vcpkg-root=vcpkg install $package
 }
