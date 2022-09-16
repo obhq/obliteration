9da5d21c875c00fee8b4ba004348633190f1ef4a
Putta Khunchalee <ultimaweapon@outlook.com>
2022-09-16 11:21:39 +0700

    Excludes ANGLE runtime from Windows artifact

--- src/post-install.cmake
+++ src/post-install.cmake
@@ -1,6 +1,11 @@
 # On Windows we need to copy Qt runtime DLLs to the application directory.
 if(WIN32)
     find_program(WINDEPLOYQT windeployqt HINTS ${Qt5_DIR}/bin)
-    execute_process(COMMAND ${WINDEPLOYQT} --release --no-translations --no-compiler-runtime ${CMAKE_INSTALL_PREFIX}/obliteration.exe
+    execute_process(COMMAND ${WINDEPLOYQT}
+        --release # Use release built binaries.
+        --no-translations # Our application is English only so skip Qt translations.
+        --no-angle # We don't use Qt Quick.
+        --no-compiler-runtime # We required user to install VC redistribution by themself.
+        ${CMAKE_INSTALL_PREFIX}/obliteration.exe
         COMMAND_ECHO STDOUT)
 endif()
