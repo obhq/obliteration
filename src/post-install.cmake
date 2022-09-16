d1c53bda4f17f0f3b437d5820d4c62c1349b9b17
Putta Khunchalee <ultimaweapon@outlook.com>
2022-09-16 10:58:16 +0700

    Excludes Qt translation from Windows artifact

--- src/post-install.cmake
+++ src/post-install.cmake
@@ -1,6 +1,6 @@
 # On Windows we need to copy Qt runtime DLLs to the application directory.
 if(WIN32)
     find_program(WINDEPLOYQT windeployqt HINTS ${Qt5_DIR}/bin)
-    execute_process(COMMAND ${WINDEPLOYQT} --release --no-compiler-runtime ${CMAKE_INSTALL_PREFIX}/obliteration.exe
+    execute_process(COMMAND ${WINDEPLOYQT} --release --no-translations --no-compiler-runtime ${CMAKE_INSTALL_PREFIX}/obliteration.exe
         COMMAND_ECHO STDOUT)
 endif()
