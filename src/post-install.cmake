793ae0d7710bd8885e1d3c553fe567ce09f70d63
Putta Khunchalee <ultimaweapon@outlook.com>
2022-09-26 13:03:59 +0700

    Excludes D3Dcompiler_47.dll and opengl32sw.dll from Windows artifact

--- src/post-install.cmake
+++ src/post-install.cmake
@@ -6,6 +6,8 @@ if(WIN32)
         --release # Use release built binaries.
         --no-quick-import # We don't use Qt Quick.
         --no-translations # Our application is English only so skip Qt translations.
+        --no-system-d3d-compiler # --no-quick-import does not implicit enable this.
+        --no-opengl-sw # Same here.
         --no-compiler-runtime # We required user to install VC redistribution by themself.
         ${CMAKE_INSTALL_PREFIX}/obliteration.exe
         COMMAND_ECHO STDOUT)
