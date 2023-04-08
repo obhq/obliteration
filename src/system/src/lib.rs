de10ffa4475a3282f3dcbe109366c419f70e1b6e
Putta Khunchalee <ultimaweapon@outlook.com>
2023-04-08 18:18:36 +0700

    Maps module name to file name when download the firmware (#186)

--- src/system/src/lib.rs
+++ src/system/src/lib.rs
@@ -1,4 +1,6 @@
+use elf::Elf;
 use error::Error;
+use fs::ModuleInfo;
 use ftp::FtpClient;
 use std::borrow::Cow;
 use std::collections::VecDeque;
@@ -62,6 +64,7 @@ pub unsafe extern "C" fn system_download(
 
     // Download the whole system directory.
     let to = Path::new(unsafe { util::str::from_c_unchecked(to) });
+    let mut modules: Vec<ModuleInfo> = Vec::new();
     let mut dirs = VecDeque::from([(String::from("/system"), to.join("system"))]);
 
     while let Some((remote, local)) = dirs.pop_front() {
@@ -90,43 +93,61 @@ pub unsafe extern "C" fn system_download(
             // Execute the action specific to the item.
             match item.ty() {
                 ItemType::RegularFile => {
-                    if let Err(e) = download_file(&mut ftp, remote, local, item.len(), status) {
+                    // Download the file.
+                    if let Err(e) = download_file(&mut ftp, &remote, &local, item.len(), status) {
                         return Error::new(&e);
                     }
+
+                    // Get module info.
+                    match get_module_info(local, remote) {
+                        Ok(v) => {
+                            if let Some(v) = v {
+                                modules.push(v);
+                            }
+                        }
+                        Err(e) => return Error::new(&e),
+                    };
                 }
                 ItemType::Directory => dirs.push_back((remote, local)),
             }
         }
     }
 
+    // Write module database.
+    if let Err(e) = write_module_db(to.join("modules.yml"), modules) {
+        return Error::new(&e);
+    }
+
     null_mut()
 }
 
-fn download_file<R>(
+fn download_file<L: AsRef<Path>, R>(
     ftp: &mut FtpClient,
-    remote: String,
-    local: PathBuf,
+    remote: &str,
+    local: L,
     len: u64,
     report: R,
 ) -> Result<(), DownloadError>
 where
     R: Fn(&str, u64, u64),
 {
+    let local = local.as_ref();
+
     // Report initial status.
     let status = format!("Downloading {remote}");
 
     report(&status, len, 0);
 
     // Create a local file.
-    let mut dst = match File::create(&local) {
+    let mut dst = match File::create(local) {
         Ok(v) => v,
-        Err(e) => return Err(DownloadError::CreateFileFailed(local, e)),
+        Err(e) => return Err(DownloadError::CreateFileFailed(local.into(), e)),
     };
 
     // Get the file.
-    let mut src = match ftp.retrieve(&remote) {
+    let mut src = match ftp.retrieve(remote) {
         Ok(v) => v,
-        Err(e) => return Err(DownloadError::RetrieveFailed(remote, e)),
+        Err(e) => return Err(DownloadError::RetrieveFailed(remote.into(), e)),
     };
 
     // Copy content.
@@ -141,7 +162,7 @@ where
                 if e.kind() == std::io::ErrorKind::Interrupted {
                     continue;
                 } else {
-                    return Err(DownloadError::ReadFailed(remote, e));
+                    return Err(DownloadError::ReadFailed(remote.into(), e));
                 }
             }
         };
@@ -152,7 +173,7 @@ where
 
         // Write to the local.
         if let Err(e) = dst.write_all(&buf[..amount]) {
-            return Err(DownloadError::WriteFailed(local, e));
+            return Err(DownloadError::WriteFailed(local.into(), e));
         }
 
         // Report status.
@@ -163,7 +184,70 @@ where
 
     // Close the remote.
     if let Err(e) = src.close() {
-        return Err(DownloadError::CloseRemoteFailed(remote, e));
+        return Err(DownloadError::CloseRemoteFailed(remote.into(), e));
+    }
+
+    Ok(())
+}
+
+fn get_module_info(local: PathBuf, remote: String) -> Result<Option<ModuleInfo>, DownloadError> {
+    use elf::OpenError;
+
+    // Check file path.
+    if !remote.starts_with("/system/common/lib/") && !remote.starts_with("/system/priv/lib/") {
+        return Ok(None);
+    }
+
+    // Check file name.
+    if let Some(ext) = local.extension() {
+        if ext == "elf" || ext == "self" {
+            return Ok(None);
+        }
+    }
+
+    // Open the file.
+    let file = match File::open(&local) {
+        Ok(v) => v,
+        Err(e) => return Err(DownloadError::OpenFileFailed(local, e)),
+    };
+
+    // Open the SELF.
+    let elf = match Elf::open(local.to_string_lossy(), file) {
+        Ok(v) => v,
+        Err(e) => match e {
+            OpenError::ReadHeaderFailed(i) if i.kind() == std::io::ErrorKind::UnexpectedEof => {
+                return Ok(None);
+            }
+            OpenError::InvalidElfMagic => return Ok(None),
+            v => return Err(DownloadError::OpenSelfFailed(local, v)),
+        },
+    };
+
+    // Check if (S)ELF has dynamic linking info.
+    let dynamic = match elf.dynamic_linking() {
+        Some(v) => v,
+        None => return Ok(None),
+    };
+
+    // Store the module information.
+    let info = dynamic.module_info();
+
+    Ok(Some(ModuleInfo {
+        name: info.name().into(),
+        path: remote,
+    }))
+}
+
+fn write_module_db(path: PathBuf, modules: Vec<ModuleInfo>) -> Result<(), DownloadError> {
+    // Open the file.
+    let file = match File::create(&path) {
+        Ok(v) => v,
+        Err(e) => return Err(DownloadError::CreateFileFailed(path, e)),
+    };
+
+    // Write the file.
+    if let Err(e) = serde_yaml::to_writer(file, &modules) {
+        return Err(DownloadError::WriteModuleDbFailed(path, e));
     }
 
     Ok(())
@@ -206,4 +290,13 @@ pub enum DownloadError {
 
     #[error("cannot close {0}")]
     CloseRemoteFailed(String, #[source] ftp::retrieve::CloseError),
+
+    #[error("cannot open {0}")]
+    OpenFileFailed(PathBuf, #[source] std::io::Error),
+
+    #[error("cannot open SELF from {0}")]
+    OpenSelfFailed(PathBuf, #[source] elf::OpenError),
+
+    #[error("cannot write a module database to {0}")]
+    WriteModuleDbFailed(PathBuf, #[source] serde_yaml::Error),
 }
