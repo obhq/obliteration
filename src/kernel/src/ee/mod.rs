e8d0e3d246835d35c39fc394145f3c29bddcb1a3
Putta Khunchalee <ultimaweapon@outlook.com>
2023-05-24 15:46:13 +0700

    Jumps to app entry point (#208)

--- src/kernel/src/ee/mod.rs
+++ src/kernel/src/ee/mod.rs
@@ -1,6 +1,10 @@
+use std::error::Error;
+
 pub mod llvm;
 #[cfg(target_arch = "x86_64")]
 pub mod native;
 
 /// An object to execute the PS4 binary.
-pub trait ExecutionEngine {}
+pub trait ExecutionEngine {
+    fn run(&mut self) -> Result<(), Box<dyn Error>>;
+}
