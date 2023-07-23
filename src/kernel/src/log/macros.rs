917c1a9641c8645fa582c50b4f0b8b0c0c864f98
Putta Khunchalee <ultimaweapon@outlook.com>
2023-07-24 02:57:38 +0700

    Revises logging system (#269)

--- src/kernel/src/log/macros.rs
+++ src/kernel/src/log/macros.rs
@@ -1,79 +1,158 @@
 /// Write the information log.
 #[macro_export]
 macro_rules! info {
-    ($logger:expr, $($arg:tt)*) => {{
-        use std::io::Write;
+    () => {
+        if let Some(l) = crate::log::LOGGER.get() {
+            let mut m = crate::log::LogMeta{
+                category: 'I',
+                color: termcolor::ColorSpec::new(),
+                file: Some(std::file!()),
+                line: Some(std::line!()),
+            };
 
-        let mut line = $logger.info();
-        write!(&mut line, $($arg)*).unwrap();
-        $logger.write(line);
-    }}
+            m.color.set_fg(Some(termcolor::Color::Cyan)).set_bold(true);
+            l.entry(m)
+        } else {
+            crate::log::LogEntry::sink()
+        }
+    };
+    ($($arg:tt)*) => {
+        if let Some(l) = crate::log::LOGGER.get() {
+            use std::io::Write;
+
+            // Setup meta.
+            let mut m = crate::log::LogMeta{
+                category: 'I',
+                color: termcolor::ColorSpec::new(),
+                file: Some(std::file!()),
+                line: Some(std::line!()),
+            };
+
+            m.color.set_fg(Some(termcolor::Color::Cyan)).set_bold(true);
+
+            // Write.
+            let mut e = l.entry(m);
+            writeln!(e, $($arg)*).unwrap();
+            l.write(e);
+        }
+    }
 }
 
 /// Write the warning log.
 #[macro_export]
 macro_rules! warn {
-    ($logger:expr, $err:ident, $($arg:tt)*) => {{
-        use std::error::Error;
-        use std::io::Write;
+    ($err:ident, $($arg:tt)*) => {
+        if let Some(l) = crate::log::LOGGER.get() {
+            use std::error::Error;
+            use std::io::Write;
 
-        // Write the message and the top-level error.
-        let mut line = $logger.warn();
+            // Setup meta.
+            let mut m = crate::log::LogMeta{
+                category: 'W',
+                color: termcolor::ColorSpec::new(),
+                file: Some(std::file!()),
+                line: Some(std::line!()),
+            };
 
-        write!(&mut line, $($arg)*).unwrap();
-        write!(&mut line, ": {}", $err).unwrap();
+            m.color.set_fg(Some(termcolor::Color::Yellow)).set_bold(true);
 
-        // Write the nested error.
-        let mut inner = $err.source();
+            // Write the message and the top-level error.
+            let mut e = l.entry(m);
 
-        while let Some(e) = inner {
-            write!(&mut line, " -> {}", e).unwrap();
-            inner = e.source();
+            write!(e, $($arg)*).unwrap();
+            write!(e, ": {}", $err).unwrap();
+
+            // Write the nested error.
+            let mut i = $err.source();
+
+            while let Some(v) = i {
+                write!(e, " -> {}", v).unwrap();
+                i = v.source();
+            }
+
+            // Print.
+            writeln!(e, ".").unwrap();
+            l.write(e);
         }
+    };
+    ($($arg:tt)*) => {
+        if let Some(l) = crate::log::LOGGER.get() {
+            use std::io::Write;
 
-        // Print.
-        write!(&mut line, ".").unwrap();
-        $logger.write(line);
-    }};
-    ($logger:expr, $($arg:tt)*) => {{
-        use std::io::Write;
+            // Setup meta.
+            let mut m = crate::log::LogMeta{
+                category: 'W',
+                color: termcolor::ColorSpec::new(),
+                file: Some(std::file!()),
+                line: Some(std::line!()),
+            };
 
-        let mut line = $logger.warn();
-        write!(&mut line, $($arg)*).unwrap();
-        $logger.write(line);
-    }}
+            m.color.set_fg(Some(termcolor::Color::Yellow)).set_bold(true);
+
+            // Write.
+            let mut e = l.entry(m);
+            writeln!(e, $($arg)*).unwrap();
+            l.write(e);
+        }
+    }
 }
 
 /// Write the error log.
 #[macro_export]
 macro_rules! error {
-    ($logger:expr, $err:ident, $($arg:tt)*) => {{
-        use std::error::Error;
-        use std::io::Write;
+    ($err:ident, $($arg:tt)*) => {
+        if let Some(l) = crate::log::LOGGER.get() {
+            #[allow(unused_imports)]
+            use std::error::Error;
+            use std::io::Write;
 
-        // Write the message and the top-level error.
-        let mut line = $logger.error();
+            // Setup meta.
+            let mut m = crate::log::LogMeta{
+                category: 'E',
+                color: termcolor::ColorSpec::new(),
+                file: Some(std::file!()),
+                line: Some(std::line!()),
+            };
 
-        write!(&mut line, $($arg)*).unwrap();
-        write!(&mut line, ": {}", $err).unwrap();
+            m.color.set_fg(Some(termcolor::Color::Red)).set_bold(true);
 
-        // Write the nested error.
-        let mut inner = $err.source();
+            // Write the message and the top-level error.
+            let mut e = l.entry(m);
 
-        while let Some(e) = inner {
-            write!(&mut line, " -> {}", e).unwrap();
-            inner = e.source();
+            write!(e, $($arg)*).unwrap();
+            write!(e, ": {}", $err).unwrap();
+
+            // Write the nested error.
+            let mut i = $err.source();
+
+            while let Some(v) = i {
+                write!(e, " -> {}", v).unwrap();
+                i = v.source();
+            }
+
+            // Print.
+            writeln!(e, ".").unwrap();
+            l.write(e);
         }
+    };
+    ($($arg:tt)*) => {
+        if let Some(l) = crate::log::LOGGER.get() {
+            use std::io::Write;
 
-        // Print.
-        write!(&mut line, ".").unwrap();
-        $logger.write(line);
-    }};
-    ($logger:expr, $($arg:tt)*) => {{
-        use std::io::Write;
+            // Setup meta.
+            let mut m = crate::log::LogMeta{
+                category: 'E',
+                color: termcolor::ColorSpec::new(),
+                file: Some(std::file!()),
+                line: Some(std::line!()),
+            };
 
-        let mut line = $logger.error();
-        write!(&mut line, $($arg)*).unwrap();
-        $logger.write(line);
-    }}
+            m.color.set_fg(Some(termcolor::Color::Red)).set_bold(true);
+
+            // Write.
+            let mut e = l.entry(m);
+            writeln!(e, $($arg)*).unwrap();
+            l.write(e);
+        }
+    }
 }
