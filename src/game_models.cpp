86333da23907a3f27bf839ce14c7874b8963a646
VocalFan <45863583+VocalFan@users.noreply.github.com>
2023-09-16 11:00:41 -0400

    Fix name sorting.

--- src/game_models.cpp
+++ src/game_models.cpp
@@ -42,6 +42,8 @@ void GameListModel::add(Game *game)
     beginInsertRows(QModelIndex(), m_items.size(), m_items.size());
     m_items.append(game);
     endInsertRows();
+
+    sortNames();
 }
 
 void GameListModel::clear()
@@ -72,4 +74,15 @@ QVariant GameListModel::data(const QModelIndex &index, int role) const
     default:
         return QVariant();
     }
+}
+
+void GameListModel::sortNames()
+{
+    beginResetModel();
+
+    std::sort(m_items.begin(), m_items.end(), [](Game *a, Game *b) {
+        return a->name() < b->name();
+    });
+
+    endResetModel();
 }
