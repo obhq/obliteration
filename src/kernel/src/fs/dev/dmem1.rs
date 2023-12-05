c251f51336f6c07b47b7cf8e8abf4bb7a6099ef2
tompro <tomas.prochazka@apertia.cz>
2023-12-06 00:17:51 +0100

    added dmem 0 - 2

---
+++ src/kernel/src/fs/dev/dmem1.rs
@@ -0,0 +1,40 @@
+use crate::errno::Errno;
+use crate::fs::{VFile, VFileOps, VPath};
+use crate::process::VThread;
+use crate::ucred::Ucred;
+use macros::vpath;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
+pub struct Dmem1 {}
+
+impl Dmem1 {
+    pub const PATH: &VPath = vpath!("/dev/dmem1");
+
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl VFileOps for Dmem1 {
+    fn write(&self, _: &VFile, _: &[u8], _: &Ucred, _: &VThread) -> Result<usize, Box<dyn Errno>> {
+        todo!()
+    }
+
+    fn ioctl(
+        &self,
+        _: &crate::fs::VFile,
+        _: u64,
+        _: &mut [u8],
+        _: &Ucred,
+        _: &VThread,
+    ) -> Result<(), Box<dyn Errno>> {
+        todo!()
+    }
+}
+
+impl Display for Dmem1 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Self::PATH.fmt(f)
+    }
+}
--- src/kernel/src/fs/dev/mod.rs
+++ src/kernel/src/fs/dev/mod.rs
@@ -1,3 +1,6 @@
 pub(super) mod console;
 pub(super) mod deci_tty6;
 pub(super) mod dipsw;
+pub(super) mod dmem0;
+pub(super) mod dmem1;
+pub(super) mod dmem2;
