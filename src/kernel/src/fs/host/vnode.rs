2b2f19f7965ecdf0508cec0e6e660234f292d12b
Putta Khunchalee <ultimaweapon@outlook.com>
2023-12-30 16:10:28 +0700

    Implements access and accessx (#538)

--- src/kernel/src/fs/host/vnode.rs
+++ src/kernel/src/fs/host/vnode.rs
@@ -1,11 +1,19 @@
 use crate::errno::Errno;
 use crate::fs::{Vnode, VopVector, DEFAULT_VNODEOPS};
+use crate::process::VThread;
+use crate::ucred::Ucred;
 use std::sync::Arc;
 
 pub static VNODE_OPS: VopVector = VopVector {
     default: Some(&DEFAULT_VNODEOPS),
+    access: Some(access),
+    accessx: None,
     lookup: Some(lookup),
 };
+
+fn access(_: &Arc<Vnode>, _: &VThread, _: &Ucred, _: u32) -> Result<(), Box<dyn Errno>> {
+    todo!()
+}
 
 fn lookup(_: &Arc<Vnode>) -> Result<Arc<Vnode>, Box<dyn Errno>> {
     todo!()
