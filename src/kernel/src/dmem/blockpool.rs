b8127dfce4a4ac81c2916a8f95013ad61d126109
tompro <tomas.prochazka@apertia.cz>
2024-01-18 20:45:22 +0100

    rename error, supress warnings

--- src/kernel/src/dmem/blockpool.rs
+++ src/kernel/src/dmem/blockpool.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_variables)]
+
 use crate::errno::{Errno, ENXIO};
 use crate::fs::{IoCmd, VFile, VFileOps};
 use crate::process::VThread;
@@ -5,8 +7,8 @@ use std::num::NonZeroI32;
 use thiserror::Error;
 
 const BLOCKPOOL_FILEOPS: VFileOps = VFileOps {
-    read: |_, _, _| Err(GenericBlockpoolError::InvalidOperation.into()),
-    write: |_, _, _| Err(GenericBlockpoolError::InvalidOperation.into()),
+    read: |_, _, _| Err(GenericError::InvalidOperation.into()),
+    write: |_, _, _| Err(GenericError::InvalidOperation.into()),
     ioctl: blockpool_ioctl,
 };
 
@@ -23,15 +25,15 @@ fn blockpool_ioctl(
 }
 
 #[derive(Debug, Error)]
-pub enum GenericBlockpoolError {
+pub enum GenericError {
     #[error("Invalid operation")]
     InvalidOperation,
 }
 
-impl Errno for GenericBlockpoolError {
+impl Errno for GenericError {
     fn errno(&self) -> NonZeroI32 {
         match self {
-            GenericBlockpoolError::InvalidOperation => ENXIO,
+            GenericError::InvalidOperation => ENXIO,
         }
     }
 }
