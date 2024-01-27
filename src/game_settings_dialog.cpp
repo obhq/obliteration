eca2352db498f56c32e592609e8fec884c5c396c
Putta Khunchalee <ultimaweapon@outlook.com>
2024-01-27 18:08:00 +0700

    Initializes game graphic settings (#577)

--- src/game_settings_dialog.cpp
+++ src/game_settings_dialog.cpp
@@ -1,9 +1,33 @@
 #include "game_settings_dialog.hpp"
+#include "game_graphic_settings.hpp"
+#include "game_models.hpp"
+
+#include <QDialogButtonBox>
+#include <QTabBar>
+#include <QTabWidget>
+#include <QVBoxLayout>
 
 GameSettingsDialog::GameSettingsDialog(Game *game, QWidget *parent) :
     QDialog(parent),
-    m_game(game)
+    m_tab(nullptr),
+    m_actions(nullptr),
+    m_graphic(nullptr)
 {
+    auto layout = new QVBoxLayout(this);
+
+    // Main tab.
+    m_tab = new QTabWidget();
+    layout->addWidget(m_tab);
+
+    // Actions bar.
+    m_actions = new QDialogButtonBox(QDialogButtonBox::Save | QDialogButtonBox::Cancel);
+    layout->addWidget(m_actions);
+
+    // Graphic tab.
+    m_graphic = new GameGraphicSettings();
+    m_tab->addTab(m_graphic, "Graphic");
+
+    setWindowTitle(game->name());
 }
 
 GameSettingsDialog::~GameSettingsDialog()
