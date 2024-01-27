30c2a3676cbcc5da9285edac92519472c1ac2908
tompro <tomas.prochazka@apertia.cz>
2024-01-27 09:44:58 +0100

    ref iter

--- src/kernel/src/ee/native/mod.rs
+++ src/kernel/src/ee/native/mod.rs
@@ -304,8 +304,8 @@ impl NativeEngine {
                 // Path the remaining with "int3".
                 target
                     .iter_mut()
-                    .take(inst.len())
                     .skip(5)
+                    .take(inst.len())
                     .for_each(|t| *t = 0xcc);
             }
             _ => todo!(
