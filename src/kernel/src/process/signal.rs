e0ecd1977cfb1ba734c121a753251fbc422aed36
SuchAFuriousDeath <48620541+SuchAFuriousDeath@users.noreply.github.com>
2024-02-24 15:49:11 +0100

    Wrapper type for signals, refactor errors and signal numbers (#595)
    Co-authored-by: tompro <tomas.prochazka@apertia.cz>

--- src/kernel/src/process/signal.rs
+++ src/kernel/src/process/signal.rs
@@ -1,5 +1,4 @@
-use crate::signal::{SigChldFlags, SignalFlags, SignalSet, SIGCHLD, SIG_MAXSIG};
-use std::num::NonZeroI32;
+use crate::signal::{SigChldFlags, Signal, SignalFlags, SignalSet, SIGCHLD, SIG_MAXSIG};
 
 /// An implementation of `sigacts` structure.
 #[derive(Debug)]
@@ -32,51 +31,51 @@ impl SignalActs {
         }
     }
 
-    pub fn handler(&self, sig: NonZeroI32) -> usize {
+    pub fn handler(&self, sig: Signal) -> usize {
         self.handler[(sig.get() - 1) as usize]
     }
 
-    pub fn set_handler(&mut self, sig: NonZeroI32, h: usize) {
+    pub fn set_handler(&mut self, sig: Signal, h: usize) {
         self.handler[(sig.get() - 1) as usize] = h;
     }
 
-    pub fn catchmask(&self, sig: NonZeroI32) -> SignalSet {
+    pub fn catchmask(&self, sig: Signal) -> SignalSet {
         self.catchmask[(sig.get() - 1) as usize]
     }
 
-    pub fn set_catchmask(&mut self, sig: NonZeroI32, mask: SignalSet) {
+    pub fn set_catchmask(&mut self, sig: Signal, mask: SignalSet) {
         self.catchmask[(sig.get() - 1) as usize] = mask;
     }
 
-    pub fn remove_stack(&mut self, sig: NonZeroI32) {
+    pub fn remove_stack(&mut self, sig: Signal) {
         self.stack.remove(sig);
     }
 
-    pub fn set_interupt(&mut self, sig: NonZeroI32) {
+    pub fn set_interupt(&mut self, sig: Signal) {
         self.interupt.add(sig);
     }
 
-    pub fn remove_reset(&mut self, sig: NonZeroI32) {
+    pub fn remove_reset(&mut self, sig: Signal) {
         self.reset.remove(sig);
     }
 
-    pub fn remove_nodefer(&mut self, sig: NonZeroI32) {
+    pub fn remove_nodefer(&mut self, sig: Signal) {
         self.nodefer.remove(sig);
     }
 
-    pub fn set_modern(&mut self, sig: NonZeroI32) {
+    pub fn set_modern(&mut self, sig: Signal) {
         self.modern.add(sig);
     }
 
-    pub fn remove_ignore(&mut self, sig: NonZeroI32) {
+    pub fn remove_ignore(&mut self, sig: Signal) {
         self.ignore.remove(sig);
     }
 
-    pub fn set_catch(&mut self, sig: NonZeroI32) {
+    pub fn set_catch(&mut self, sig: Signal) {
         self.catch.add(sig);
     }
 
-    pub fn remove_catch(&mut self, sig: NonZeroI32) {
+    pub fn remove_catch(&mut self, sig: Signal) {
         self.catch.remove(sig);
     }
 
@@ -88,7 +87,7 @@ impl SignalActs {
         self.flag = flag;
     }
 
-    pub fn signal_flags(&self, sig: NonZeroI32) -> SignalFlags {
+    pub fn signal_flags(&self, sig: Signal) -> SignalFlags {
         let mut flags: SignalFlags = SignalFlags::empty();
 
         if self.stack.contains(sig) {
--- src/kernel/src/signal/set.rs
+++ src/kernel/src/signal/set.rs
@@ -1,6 +1,5 @@
-use super::strsignal;
+use super::{strsignal, Signal, SignalIter};
 use std::fmt::{Display, Formatter};
-use std::num::NonZeroI32;
 use std::ops::{BitAndAssign, BitOrAssign, Not};
 
 /// An implementation of `sigset_t`.
@@ -12,32 +11,33 @@ pub struct SignalSet {
 
 impl SignalSet {
     /// An implementation of `SIGISMEMBER`.
-    pub fn contains(&self, sig: NonZeroI32) -> bool {
+    pub fn contains(&self, sig: Signal) -> bool {
         (self.bits[Self::word(sig)] & Self::bit(sig)) != 0
     }
 
     /// An implementation of `SIGADDSET`.
-    pub fn add(&mut self, sig: NonZeroI32) {
+    pub fn add(&mut self, sig: Signal) {
         self.bits[Self::word(sig)] |= Self::bit(sig);
     }
 
     /// An implementation of `SIGDELSET`.
-    pub fn remove(&mut self, sig: NonZeroI32) {
+    pub fn remove(&mut self, sig: Signal) {
         self.bits[Self::word(sig)] &= !Self::bit(sig);
     }
 
     // An implementation of `_SIG_IDX`.
-    fn idx(s: NonZeroI32) -> i32 {
+    fn idx(s: Signal) -> i32 {
         s.get() - 1
     }
 
     /// An implementation of `_SIG_WORD`.
-    fn word(s: NonZeroI32) -> usize {
-        (Self::idx(s) >> 5).try_into().unwrap()
+    fn word(s: Signal) -> usize {
+        // This is safe because `Signal` is guaranteed to be non-negative.
+        unsafe { (Self::idx(s) >> 5).try_into().unwrap() }
     }
 
     /// An implementation of `_SIG_BIT`.
-    fn bit(s: NonZeroI32) -> u32 {
+    fn bit(s: Signal) -> u32 {
         1 << (Self::idx(s) & 31)
     }
 }
@@ -73,17 +73,13 @@ impl Display for SignalSet {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut first = true;
 
-        for i in 1..=128 {
-            let num = unsafe { NonZeroI32::new_unchecked(i) };
-
-            if self.contains(num) {
-                if !first {
-                    f.write_str(" | ")?;
-                }
-
-                f.write_str(strsignal(num).as_ref())?;
-                first = false;
+        for sig in SignalIter::new().filter(|sig| self.contains(*sig)) {
+            if !first {
+                f.write_str(" | ")?;
             }
+
+            f.write_str(strsignal(sig).as_ref())?;
+            first = false;
         }
 
         if first {
