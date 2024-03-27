8eb422ee6fcfe0408a8ea3f4a9a7c300f80dee88
Tomáš Procházka <thompson2001@email.cz>
2024-03-27 10:58:53 +0100

    Revert "Fixes mkdir on Windows"
    This reverts commit a6652078dfdf3766bd258eb95baf71b6e01125e2.

--- src/kernel/src/fs/host/file.rs
+++ src/kernel/src/fs/host/file.rs
@@ -209,7 +209,7 @@ impl HostFile {
         use std::ptr::null_mut;
         use windows_sys::Wdk::{
             Foundation::OBJECT_ATTRIBUTES,
-            Storage::FileSystem::{NtCreateFile, FILE_CREATE, FILE_DIRECTORY_FILE},
+            Storage::FileSystem::{NtCreateFile, FILE_DIRECTORY_FILE, FILE_OPEN},
         };
         use windows_sys::Win32::{
             Foundation::{RtlNtStatusToDosError, STATUS_SUCCESS, UNICODE_STRING},
@@ -256,7 +256,7 @@ impl HostFile {
                 null_mut(),
                 FILE_ATTRIBUTE_DIRECTORY,
                 FILE_SHARE_READ | FILE_SHARE_WRITE,
-                FILE_CREATE,
+                FILE_OPEN,
                 FILE_DIRECTORY_FILE,
                 null_mut(),
                 0,
