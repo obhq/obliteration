50c5ab747dec8f9de305d93c5457b34c762049a3
VocalFan <45863583+VocalFan@users.noreply.github.com>
2024-04-11 10:31:32 -0400

    Add mdbg and change how namedobj_create works for compatibility

--- src/kernel/src/namedobj/mod.rs
+++ src/kernel/src/namedobj/mod.rs
@@ -11,17 +11,20 @@ impl NamedObjManager {
     pub fn new(sys: &mut Syscalls) -> Arc<Self> {
         let namedobj = Arc::new(Self {});
 
-        sys.register(557, &namedobj, Self::sys_namedobj_create);
+        sys.register(557, &Arc::clone(&namedobj), Self::sys_namedobj_create);
+
+        sys.register(601, &Arc::clone(&namedobj), Self::sys_mdbg_service);
 
         namedobj
     }
 
-    fn sys_namedobj_create(self: &Arc<Self>, td: &VThread, i: &SysIn) -> Result<SysOut, SysErr> {
-        // Get arguments.
-        let name = unsafe { i.args[0].to_str(32) }?.ok_or(SysErr::Raw(EINVAL))?;
-        let data: usize = i.args[1].into();
-        let flags: u32 = i.args[2].try_into().unwrap();
-
+   fn namedobj_create(
+        self: &Self,
+        td: &VThread,
+        name: &str,
+        data: usize,
+        flags: u32,
+    ) -> Result<SysOut, SysErr> {
         // Allocate the entry.
         let mut table = td.proc().objects_mut();
 
@@ -42,7 +45,35 @@ impl NamedObjManager {
 
         Ok(id.into())
     }
-}
+
+    pub fn sys_namedobj_create(
+        self: &Arc<Self>,
+        td: &VThread,
+        i: &SysIn,
+    ) -> Result<SysOut, SysErr> {
+        // Get arguments to pass to namedobj_create
+        let name = unsafe { i.args[0].to_str(32) }?.ok_or(SysErr::Raw(EINVAL))?;
+        let data: usize = i.args[1].into();
+        let flags: u32 = i.args[2].try_into().unwrap();
+
+        self.namedobj_create(td, name, data, flags)
+    }
+
+    fn sys_mdbg_service(self: &Arc<Self>, td: &VThread, i: &SysIn) -> Result<SysOut, SysErr> {
+        // Get the mdbg arg
+        let mdbg_type: i32 = i.args[0].try_into().unwrap();
+        if mdbg_type == 0x1 {
+            // Get arguments for namedobj creation
+            let name = unsafe { i.args[1].to_str(32) }?.ok_or(SysErr::Raw(EINVAL))?;
+            let data: usize = i.args[2].into();
+            let flags: u32 = i.args[3].try_into().unwrap();
+
+            self.namedobj_create(td, name, data, flags)
+        } else {
+            info!("mdbg_service with value {:?}", mdbg_type);
+            Ok(SysOut::ZERO)
+        }
+    }
 
 #[derive(Debug)]
 pub struct NamedObj {
