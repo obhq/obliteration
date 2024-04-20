e5359e1dad7e98e223e36d24a2d2e7cf2ea1e982
tompro <tomas.prochazka@apertia.cz>
2024-04-20 14:50:41 +0200

    Creates vcpus on linux

--- src/kernel/src/hv/linux/kvm.cpp
+++ src/kernel/src/hv/linux/kvm.cpp
@@ -66,3 +66,16 @@ extern "C" int kvm_set_user_memory_region(
 
     return 0;
 }
+
+extern "C" int kvm_create_vcpu(int vm, int id, int *fd)
+{
+    auto vcpu = ioctl(vm, KVM_CREATE_VCPU, id);
+
+    if (vcpu < 0) {
+        return errno;
+    }
+
+    *fd = vcpu;
+
+    return 0;
+}
