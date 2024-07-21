49f54b528d3cb895b3627d836a4d02a6289268ac
Putta Khunchalee <ultimaweapon@outlook.com>
2024-07-21 20:54:07 +0700

    Fixes hv_vm_create mac Mac Intel

--- src/core/src/vmm/macos/vm.rs
+++ src/core/src/vmm/macos/vm.rs
@@ -8,7 +8,10 @@ pub struct Vm(());
 
 impl Vm {
     pub fn new() -> Result<Self, NonZero<c_int>> {
+        #[cfg(target_arch = "aarch64")]
         let ret = unsafe { hv_vm_create(null_mut()) };
+        #[cfg(target_arch = "x86_64")]
+        let ret = unsafe { hv_vm_create(0) };
 
         match NonZero::new(ret) {
             Some(ret) => Err(ret),
