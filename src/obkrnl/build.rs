1bbf9d2f8a557b677bde9a00c17075f96c4776bf
Putta Khunchalee <ultimaweapon@outlook.com>
2024-07-22 18:30:07 +0700

    Fixes kernel-specific linker options also applied to unit tests

--- src/obkrnl/build.rs
+++ src/obkrnl/build.rs
@@ -1,4 +1,8 @@
 fn main() {
-    println!("cargo::rustc-link-arg-bin=obkrnl=-zcommon-page-size=0x4000");
-    println!("cargo::rustc-link-arg-bin=obkrnl=-zmax-page-size=0x4000");
+    let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    if os == "none" {
+        println!("cargo::rustc-link-arg-bins=-zcommon-page-size=0x4000");
+        println!("cargo::rustc-link-arg-bins=-zmax-page-size=0x4000");
+    }
 }
