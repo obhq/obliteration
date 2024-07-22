219c2355cd0df825bcfe9eaf9801d00c15355136
Putta Khunchalee <ultimaweapon@outlook.com>
2024-07-22 18:20:34 +0700

    Fixes broken tests on macOS

--- src/obkrnl/build.rs
+++ src/obkrnl/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    println!("cargo::rustc-link-arg-bins=-zcommon-page-size=0x4000");
-    println!("cargo::rustc-link-arg-bins=-zmax-page-size=0x4000");
+    println!("cargo::rustc-link-arg-bin=obkrnl=-zcommon-page-size=0x4000");
+    println!("cargo::rustc-link-arg-bin=obkrnl=-zmax-page-size=0x4000");
 }
