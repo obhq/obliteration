60abef4225441fc7797e6417b5285779ef09efb3
Putta Khunchalee <ultimaweapon@outlook.com>
2024-07-29 22:12:23 +0700

    Fixes build error on AArch64

--- src/core/src/vmm/macos/cpu.rs
+++ src/core/src/vmm/macos/cpu.rs
@@ -1,4 +1,4 @@
-use crate::vmm::{Cpu, CpuExit, CpuIo, CpuStates};
+use crate::vmm::{Cpu, CpuExit, CpuStates};
 use hv_sys::hv_vcpu_destroy;
 use std::marker::PhantomData;
 use thiserror::Error;
@@ -134,7 +134,7 @@ impl<'a> CpuExit for HfExit<'a> {
     }
 
     #[cfg(target_arch = "x86_64")]
-    fn is_io(&mut self) -> Option<CpuIo> {
+    fn is_io(&mut self) -> Option<crate::vmm::CpuIo> {
         todo!()
     }
 }
