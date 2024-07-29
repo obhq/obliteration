dd89fc4e0589fcde1929ea5205a6816bbc83e96f
tompro <tomas.prochazka@apertia.cz>
2024-07-29 22:06:29 +0200

    regs

--- src/core/src/vmm/macos/cpu.rs
+++ src/core/src/vmm/macos/cpu.rs
@@ -97,47 +97,36 @@ impl<'a> Cpu for HfCpu<'a> {
 
     #[cfg(target_arch = "x86_64")]
     fn states(&mut self) -> Result<Self::States<'_>, Self::GetStatesErr> {
-        let rsp = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_RIP)
-            .map_err(GetStatesError::ReadRsp)?;
-        let rip = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_RSP)
-            .map_err(GetStatesError::ReadRip)?;
-        let cr0 = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_CR0)
+        let cr0 = self.read_register(hv_sys::hv_x86_reg_t_HV_X86_CR0)
             .map_err(GetStatesError::ReadCr0)?;
-        let cr3 = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_CR3)
-            .map_err(GetStatesError::ReadCr3)?;
-        let cr4 = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_CR4)
+        let cr4 = self.read_register(hv_sys::hv_x86_reg_t_HV_X86_CR4)
             .map_err(GetStatesError::ReadCr4)?;
-        let ds = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_DS)
-            .map_err(GetStatesError::ReadDs)?;
-        let es = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_ES)
-            .map_err(GetStatesError::ReadEs)?;
-        let fs = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_FS)
-            .map_err(GetStatesError::ReadFs)?;
-        let gs = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_GS)
-            .map_err(GetStatesError::ReadGs)?;
-        let ss = self
-            .read_register(hv_sys::hv_x86_reg_t_HV_X86_SS)
-            .map_err(GetStatesError::ReadSs)?;
+
+        let mut cs = 0u64;
+        let mut ds = 0u64;
+        let mut es = 0u64;
+        let mut fs = 0u64;
+        let mut gs = 0u64;
+        let mut ss = 0u64;
+
+        unsafe {
+            wrap_return!(hv_sys::hv_vmx_vcpu_read_vmcs(self.instance, hv_sys::VMCS_GUEST_CS_AR, &mut cs), GetStatesError::ReadCs)?;
+            wrap_return!(hv_sys::hv_vmx_vcpu_read_vmcs(self.instance, hv_sys::VMCS_GUEST_DS_AR, &mut ds), GetStatesError::ReadDs)?;
+            wrap_return!(hv_sys::hv_vmx_vcpu_read_vmcs(self.instance, hv_sys::VMCS_GUEST_ES_AR, &mut es), GetStatesError::ReadEs)?;
+            wrap_return!(hv_sys::hv_vmx_vcpu_read_vmcs(self.instance, hv_sys::VMCS_GUEST_FS_AR, &mut fs), GetStatesError::ReadFs)?;
+            wrap_return!(hv_sys::hv_vmx_vcpu_read_vmcs(self.instance, hv_sys::VMCS_GUEST_GS_AR, &mut gs), GetStatesError::ReadGs)?;
+            wrap_return!(hv_sys::hv_vmx_vcpu_read_vmcs(self.instance, hv_sys::VMCS_GUEST_SS_AR, &mut ss), GetStatesError::ReadSs)?;
+        }
 
         Ok(HfStates {
             cpu: self,
-            dirty: false,
-
-            rsp,
-            rip,
+            dirty_flags: DirtyFlags::empty(),
+            rsp: 0,
+            rip: 0,
             cr0,
-            cr3,
+            cr3: 0,
             cr4,
-
+            cs,
             ds,
             es,
             fs,
@@ -187,10 +176,26 @@ impl<'a> Drop for HfCpu<'a> {
     }
 }
 
+bitflags::bitflags! {
+    struct DirtyFlags: u16 {
+        const RSP  = 0x0001;
+        const RIP  = 0x0002;
+        const CR0  = 0x0004;
+        const CR3  = 0x0008;
+        const CR4  = 0x0010;
+        const CS   = 0x0020;
+        const DS   = 0x0040;
+        const ES   = 0x0080;
+        const FS   = 0x0100;
+        const GS   = 0x0200;
+        const SS   = 0x0400;
+    }
+}
+
 /// Implementation of [`Cpu::States`] for Hypervisor Framework.
 pub struct HfStates<'a, 'b> {
     cpu: &'a mut HfCpu<'b>,
-    dirty: bool,
+    dirty_flags: DirtyFlags,
 
     rsp: usize,
     rip: usize,
@@ -199,6 +204,7 @@ pub struct HfStates<'a, 'b> {
     cr3: usize,
     cr4: usize,
 
+    cs: u64,
     ds: usize,
     es: usize,
     fs: usize,
@@ -210,91 +216,138 @@ impl<'a, 'b> CpuStates for HfStates<'a, 'b> {
     #[cfg(target_arch = "x86_64")]
     fn set_rsp(&mut self, v: usize) {
         self.rsp = v;
-        self.dirty = true;
+        self.dirty_flags.insert(DirtyFlags::RSP);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_rip(&mut self, v: usize) {
         self.rip = v;
-        self.dirty = true;
+        self.dirty_flags.insert(DirtyFlags::RIP);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_cr0(&mut self, v: usize) {
         self.cr0 = v;
-        self.dirty = true;
+        self.dirty_flags.insert(DirtyFlags::CR0);
     }
 
     #[cfg(target_arch = "x86_64")]
-    fn set_cr3(&mut self, v: usize) {
-        self.cr3 = v;
-        self.dirty = true;
+    fn set_cr0(&mut self, v: usize) {
+        // Preserve certain bits that should not be modified
+        const FIXED_BITS: usize = 0x60000010; // PE, NE, ET, and PG bits
+        self.cr0 = (self.cr0 & FIXED_BITS) | (v & !FIXED_BITS);
+        self.dirty_flags.insert(DirtyFlags::CR0);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_cr4(&mut self, v: usize) {
-        self.cr4 = v;
-        self.dirty = true;
+        // Preserve certain bits that should not be modified
+        const FIXED_BITS: usize = 0x2000; // VMXE bit
+        self.cr4 = (self.cr4 & FIXED_BITS) | (v & !FIXED_BITS);
+        self.dirty_flags.insert(DirtyFlags::CR4);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_efer(&mut self, v: usize) {
-        todo!()
+        // LME and LMA bits
+        // There seems to be now way to actually set these, however they appear to be set by default
+        assert_eq!(v, 0x100 | 0x400);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_cs(&mut self, ty: u8, dpl: u8, p: bool, l: bool, d: bool) {
-        todo!()
+        let new_ar = 0x9b | // Present, Code, Readable
+                 ((ty as u64) << 8) |
+                 ((dpl as u64) << 5) |
+                 ((p as u64) << 7) |
+                 ((l as u64) << 13) |
+                 ((d as u64) << 14);
+
+        // Preserve bits that should not be modified
+        const FIXED_BITS: u64 = 0xFFFFF000; // Preserve upper 20 bits
+        self.cs = (self.cs & FIXED_BITS) | (new_ar & !FIXED_BITS);
+        self.dirty_flags.insert(DirtyFlags::CS);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_ds(&mut self, p: bool) {
-        todo!()
+        self.ds = if p { 0x0093 } else { 0x0000 };
+        self.dirty_flags.insert(DirtyFlags::DS);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_es(&mut self, p: bool) {
-        todo!()
+        self.es = if p { 0x0093 } else { 0x0000 };
+        self.dirty_flags.insert(DirtyFlags::ES);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_fs(&mut self, p: bool) {
-        todo!()
+        self.fs = if p { 0x0093 } else { 0x0000 };
+        self.dirty_flags.insert(DirtyFlags::FS);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_gs(&mut self, p: bool) {
-        todo!()
+        self.gs = if p { 0x0093 } else { 0x0000 };
+        self.dirty_flags.insert(DirtyFlags::GS);
     }
 
     #[cfg(target_arch = "x86_64")]
     fn set_ss(&mut self, p: bool) {
-        todo!()
+        self.ss = if p { 0x0093 } else { 0x0000 };
+        self.dirty_flags.insert(DirtyFlags::SS);
     }
 }
 
 impl<'a, 'b> Drop for HfStates<'a, 'b> {
     #[cfg(target_arch = "x86_64")]
     fn drop(&mut self) {
-        if !self.dirty {
+        if self.dirty_flags.is_empty() {
             return;
         }
 
-        self.cpu
-            .write_register(hv_sys::hv_x86_reg_t_HV_X86_RIP, self.rip)
-            .unwrap();
-        self.cpu
-            .write_register(hv_sys::hv_x86_reg_t_HV_X86_RSP, self.rsp)
-            .unwrap();
-        self.cpu
-            .write_register(hv_sys::hv_x86_reg_t_HV_X86_CR0, self.cr0)
-            .unwrap();
-        self.cpu
-            .write_register(hv_sys::hv_x86_reg_t_HV_X86_CR3, self.cr3)
-            .unwrap();
-        self.cpu
-            .write_register(hv_sys::hv_x86_reg_t_HV_X86_CR4, self.cr4)
-            .unwrap();
+        if self.dirty_flags.contains(DirtyFlags::RIP) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_RIP, self.rip).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::RSP) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_RSP, self.rsp).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::CR0) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_CR0, self.cr0).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::CR3) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_CR3, self.cr3).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::CR4) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_CR4, self.cr4).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::EFER) {
+            unsafe { hv_sys::hv_vmx_vcpu_write_vmcs(self.cpu.instance, hv_sys::VMCS_GUEST_IA32_EFER, self.efer as u64) }.unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::CS) {
+            unsafe {
+                hv_sys::hv_vmx_vcpu_write_vmcs(self.cpu.instance, hv_sys::VMCS_GUEST_CS, 0).unwrap();
+                hv_sys::hv_vmx_vcpu_write_vmcs(self.cpu.instance, hv_sys::VMCS_GUEST_CS_BASE, 0).unwrap();
+                hv_sys::hv_vmx_vcpu_write_vmcs(self.cpu.instance, hv_sys::VMCS_GUEST_CS_LIMIT, 0xffffffff).unwrap();
+                hv_sys::hv_vmx_vcpu_write_vmcs(self.cpu.instance, hv_sys::VMCS_GUEST_CS_AR, self.cs).unwrap();
+            }
+        }
+        if self.dirty_flags.contains(DirtyFlags::DS) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_DS, self.ds).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::ES) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_ES, self.es).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::FS) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_FS, self.fs).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::GS) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_GS, self.gs).unwrap();
+        }
+        if self.dirty_flags.contains(DirtyFlags::SS) {
+            self.cpu.write_register(hv_sys::hv_x86_reg_t_HV_X86_SS, self.ss).unwrap();
+        }
     }
 
     #[cfg(target_arch = "aarch64")]
@@ -330,43 +383,27 @@ pub enum RunError {
 /// Implementation of [`Cpu::GetStatesErr`].
 #[derive(Debug, Error)]
 pub enum GetStatesError {
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading rsp: {0:#x}")]
-    ReadRsp(NonZero<hv_sys::hv_return_t>),
-
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading rip: {0:#x}")]
-    ReadRip(NonZero<hv_sys::hv_return_t>),
-
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading cr0: {0:#x}")]
+    #[error("error while reading CR0: {0:#x}")]
     ReadCr0(NonZero<hv_sys::hv_return_t>),
 
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading cr3: {0:#x}")]
-    ReadCr3(NonZero<hv_sys::hv_return_t>),
-
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading cr4: {0:#x}")]
+    #[error("error while reading CR4: {0:#x}")]
     ReadCr4(NonZero<hv_sys::hv_return_t>),
 
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading ds: {0:#x}")]
+    #[error("error while reading CS: {0:#x}")]
+    ReadCs(NonZero<hv_sys::hv_return_t>),
+
+    #[error("error while reading DS: {0:#x}")]
     ReadDs(NonZero<hv_sys::hv_return_t>),
 
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading es: {0:#x}")]
+    #[error("error while reading ES: {0:#x}")]
     ReadEs(NonZero<hv_sys::hv_return_t>),
 
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading fs: {0:#x}")]
+    #[error("error while reading FS: {0:#x}")]
     ReadFs(NonZero<hv_sys::hv_return_t>),
 
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading gs: {0:#x}")]
+    #[error("error while reading GS: {0:#x}")]
     ReadGs(NonZero<hv_sys::hv_return_t>),
 
-    #[cfg(target_arch = "x86_64")]
-    #[error("error while reading ss: {0:#x}")]
+    #[error("error while reading SS: {0:#x}")]
     ReadSs(NonZero<hv_sys::hv_return_t>),
 }
