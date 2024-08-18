079304c95bfb692221a747c588ed693951493ff9
Putta Khunchalee <ultimaweapon@outlook.com>
2024-08-18 21:00:22 +0700

    Fixes build error on AArch64

--- src/obkrnl/src/config/mod.rs
+++ src/obkrnl/src/config/mod.rs
@@ -2,9 +2,13 @@ use core::ptr::null;
 use macros::elf_note;
 use obconf::BootEnv;
 
+#[cfg(target_arch = "aarch64")]
+pub use self::aarch64::*;
 #[cfg(target_arch = "x86_64")]
 pub use self::x86_64::*;
 
+#[cfg(target_arch = "aarch64")]
+mod aarch64;
 #[cfg(target_arch = "x86_64")]
 mod x86_64;
 
