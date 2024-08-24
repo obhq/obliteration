006ee35349d17a7ff31b55431ec10e21626a2300
Putta Khunchalee <ultimaweapon@outlook.com>
2024-08-24 19:15:57 +0700

    Revises console

--- src/core/src/vmm/hw/console/mod.rs
+++ src/core/src/vmm/hw/console/mod.rs
@@ -1,9 +1,8 @@
 use self::context::Context;
 use super::{Device, DeviceContext, Ram};
-use obvirt::console::{Memory, MsgType};
-use std::collections::VecDeque;
+use crate::vmm::VmmEventHandler;
+use obvirt::console::Memory;
 use std::num::NonZero;
-use std::sync::Mutex;
 
 mod context;
 
@@ -11,11 +10,11 @@ mod context;
 pub struct Console {
     addr: usize,
     len: NonZero<usize>,
-    logs: Mutex<VecDeque<Log>>,
+    event: VmmEventHandler,
 }
 
 impl Console {
-    pub fn new(addr: usize, vm_page_size: NonZero<usize>) -> Self {
+    pub fn new(addr: usize, vm_page_size: NonZero<usize>, event: VmmEventHandler) -> Self {
         let len = size_of::<Memory>()
             .checked_next_multiple_of(vm_page_size.get())
             .and_then(NonZero::new)
@@ -23,11 +22,7 @@ impl Console {
 
         addr.checked_add(len.get()).unwrap();
 
-        Self {
-            addr,
-            len,
-            logs: Mutex::default(),
-        }
+        Self { addr, len, event }
     }
 }
 
@@ -43,12 +38,4 @@ impl Device for Console {
     fn create_context<'a>(&'a self, ram: &'a Ram) -> Box<dyn DeviceContext + 'a> {
         Box::new(Context::new(self, ram))
     }
-}
-
-/// Contains data for each logging entry.
-struct Log {
-    ty: MsgType,
-    file: String,
-    line: u32,
-    msg: String,
 }
--- src/obvirt/src/console/mod.rs
+++ src/obvirt/src/console/mod.rs
@@ -2,41 +2,14 @@
 ///
 /// The sequence of operations on a console memory is per-cpu. The kernel will start each log by:
 ///
-/// 1. Write [`Self::file_len`] then [`Self::file_addr`].
-/// 2. Write [`Self::msg_len`] then [`Self::msg_addr`].
-/// 3. Repeat step 2 until the whole message has been written.
-/// 4. Write [`Self::commit`].
+/// 1. Write [`Self::msg_len`] then [`Self::msg_addr`].
+/// 2. Repeat step 1 until the whole message has been written.
+/// 3. Write [`Self::commit`].
 #[repr(C)]
 pub struct Memory {
-    pub file_len: usize,
-    pub file_addr: usize,
     pub msg_len: usize,
     pub msg_addr: usize,
-    pub commit: Commit,
-}
-
-/// Struct to commit a log.
-#[repr(transparent)]
-pub struct Commit(u32);
-
-impl Commit {
-    /// # Panics
-    /// If `line` greater than 0xffffff.
-    pub fn new(ty: MsgType, line: u32) -> Self {
-        assert!(line <= 0xffffff);
-
-        Self((ty as u32) << 24 | line)
-    }
-
-    pub fn parse(raw: u32) -> Option<(MsgType, u32)> {
-        let line = raw & 0xffffff;
-        let ty = match (raw >> 24) as u8 {
-            v if v == MsgType::Info as u8 => MsgType::Info,
-            _ => return None,
-        };
-
-        Some((ty, line))
-    }
+    pub commit: MsgType,
 }
 
 /// Type of console message.
@@ -44,4 +17,15 @@ impl Commit {
 #[derive(Clone, Copy)]
 pub enum MsgType {
     Info,
+}
+
+impl MsgType {
+    pub fn from_u8(v: u8) -> Option<Self> {
+        let v = match v {
+            v if v == MsgType::Info as u8 => MsgType::Info,
+            _ => return None,
+        };
+
+        Some(v)
+    }
 }
