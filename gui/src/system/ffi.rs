7475e98cd0e2353e6fc326e9e807bf0eacfdbdbe
tompro <tomas.prochazka@apertia.cz>
2024-11-17 11:24:02 +0100

    ref

--- gui/src/system/ffi.rs
+++ gui/src/system/ffi.rs
@@ -1,5 +1,5 @@
 use crate::error::RustError;
-use std::ffi::{c_char, c_void};
+use std::ffi::{c_char, c_void, CStr};
 use std::ptr::null_mut;
 
 #[no_mangle]
@@ -9,5 +9,10 @@ pub unsafe extern "C-unwind" fn update_firmware(
     cx: *mut c_void,
     status: unsafe extern "C-unwind" fn(*const c_char, u64, u64, *mut c_void),
 ) -> *mut RustError {
+    let _root_path = CStr::from_ptr(root);
+    let _fw_path = CStr::from_ptr(fw);
+    let _cx = cx;
+    let _status = status;
+
     null_mut()
 }
