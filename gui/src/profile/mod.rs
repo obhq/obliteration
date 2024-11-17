7236dd93b46a2a01af5628fd93909b56d9796dea
tompro <tomas.prochazka@apertia.cz>
2024-11-18 00:21:01 +0100

    red

--- gui/src/profile/mod.rs
+++ gui/src/profile/mod.rs
@@ -60,7 +60,7 @@ impl Default for Profile {
     fn default() -> Self {
         Self {
             id: Uuid::new_v4(),
-            name: CString::new("Default").unwrap(),
+            name: CString::from(c"Default"),
             display_resolution: DisplayResolution::Hd,
             kernel_config: Config {
                 max_cpu: NonZero::new(8).unwrap(),
