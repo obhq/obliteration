adc2b260f540e0f66f60f11604276562c35eb8fe
Putta Khunchalee <ultimaweapon@outlook.com>
2024-12-01 19:41:28 +0700

    Links resources.rc

--- gui/build.rs
+++ gui/build.rs
@@ -3,15 +3,18 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 
 fn main() {
-    // Build path for @root.
-    let mut root = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
-
-    root.push("ui");
+    let root = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
 
     // Compile Slint.
     let config = CompilerConfiguration::new()
         .with_style(String::from("fluent-dark"))
-        .with_library_paths(HashMap::from([("root".into(), root)]));
+        .with_library_paths(HashMap::from([("root".into(), root.join("ui"))]));
 
     slint_build::compile_with_config(PathBuf::from_iter(["ui", "main.slint"]), config).unwrap();
+
+    // Compile resources.rc.
+    #[cfg(windows)]
+    winres::WindowsResource::new()
+        .set_resource_file(root.join("resources.rc").to_str().unwrap())
+        .compile();
 }
