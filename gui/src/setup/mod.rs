0554198cf4bd4af3c9bc6692454dcaf8e5dcaba4
Putta Khunchalee <ultimaweapon@outlook.com>
2024-12-06 02:04:30 +0700

    Implements firmware extraction

--- gui/src/setup/mod.rs
+++ gui/src/setup/mod.rs
@@ -5,7 +5,7 @@ use crate::data::{DataError, DataMgr};
 use crate::dialogs::{open_dir, open_file, FileType};
 use crate::ui::SetupWizard;
 use erdp::ErrorDisplay;
-use obfw::ps4::PartReader;
+use obfw::ps4::{PartData, PartReader};
 use obfw::{DumpReader, ItemReader};
 use slint::{ComponentHandle, PlatformError};
 use std::cell::Cell;
@@ -201,6 +201,22 @@ fn install_firmware(win: SetupWizard) {
         }
     };
 
+    // Create data manager to see if path is writable.
+    let root = win.get_data_root();
+    let dmgr = match DataMgr::new(root.as_str()) {
+        Ok(v) => v,
+        Err(e) => {
+            let m = format!(
+                "Failed to create data manager on {}: {}.",
+                root,
+                e.display()
+            );
+
+            win.set_error_message(m.into());
+            return;
+        }
+    };
+
     win.invoke_show_firmware_installer();
     win.set_firmware_status("Initializing...".into());
 
@@ -214,6 +230,7 @@ fn install_firmware(win: SetupWizard) {
         let e =
             match extract_firmware_dump(
                 &mut dump,
+                &dmgr,
                 |v| drop(win.upgrade_in_event_loop(move |w| w.set_firmware_status(v.into()))),
                 || {
                     p += 1;
@@ -242,6 +259,7 @@ fn install_firmware(win: SetupWizard) {
 
 fn extract_firmware_dump(
     dump: &mut DumpReader<File>,
+    dmgr: &DataMgr,
     mut status: impl FnMut(String),
     mut step: impl FnMut(),
 ) -> Result<(), FirmwareError> {
@@ -258,8 +276,10 @@ fn extract_firmware_dump(
         status(format!("Extracting {name}..."));
 
         // Extract item.
-        let r = match &mut item {
-            ItemReader::Ps4Part(r) => extract_partition(r),
+        let r: Result<(), Box<dyn Error>> = match &mut item {
+            ItemReader::Ps4Part(r) => {
+                extract_partition(dmgr, r, &mut status, &mut step).map_err(|e| e.into())
+            }
         };
 
         if let Err(e) = r {
@@ -272,8 +292,98 @@ fn extract_firmware_dump(
     Ok(())
 }
 
-fn extract_partition(part: &mut PartReader<File>) -> Result<(), Box<dyn Error>> {
-    todo!()
+fn extract_partition(
+    dmgr: &DataMgr,
+    part: &mut PartReader<File>,
+    status: &mut impl FnMut(String),
+    step: &mut impl FnMut(),
+) -> Result<(), PartitionError> {
+    // Get device path.
+    let dev = part.dev();
+    let dev = match std::str::from_utf8(dev) {
+        Ok(v) => v,
+        Err(_) => {
+            let n = String::from_utf8_lossy(dev);
+            return Err(PartitionError::UnexpectedDevice(n.into_owned()));
+        }
+    };
+
+    // Get device name.
+    let dev = if dev == "md0" {
+        dev
+    } else if let Some(v) = dev.strip_prefix("/dev/") {
+        if v.contains(['/', '\\']) {
+            return Err(PartitionError::UnexpectedDevice(dev.into()));
+        }
+
+        v
+    } else {
+        return Err(PartitionError::UnexpectedDevice(dev.into()));
+    };
+
+    // Extract items.
+    let root = dmgr.part().data(dev);
+
+    loop {
+        // Get next item.
+        let item = match part.next_item().map_err(PartitionError::NextItem)? {
+            Some(v) => v,
+            None => break,
+        };
+
+        // Unpack item.
+        let (name, data) = match item {
+            PartData::Directory(n) => (n, None),
+            PartData::File(n, r) => (n, Some(r)),
+        };
+
+        // Get name.
+        let name = match String::from_utf8(name) {
+            Ok(v) => v,
+            Err(e) => {
+                let n = String::from_utf8_lossy(e.as_bytes());
+                return Err(PartitionError::UnexpectedFile(n.into_owned()));
+            }
+        };
+
+        // Get local path.
+        let mut path = root.to_path_buf();
+
+        for com in name.split('/').skip(1) {
+            if com.is_empty() || com.contains('\\') {
+                return Err(PartitionError::UnexpectedFile(name));
+            }
+
+            path.push(com);
+        }
+
+        // Extract item.
+        match data {
+            Some(mut data) => {
+                status(format!("Extracting {name}..."));
+
+                // Create only if not exists.
+                let mut file = match File::create_new(&path) {
+                    Ok(v) => v,
+                    Err(e) => return Err(PartitionError::CreateFile(path, e)),
+                };
+
+                if let Err(e) = std::io::copy(&mut data, &mut file) {
+                    return Err(PartitionError::ExtractFile(name, path, e));
+                }
+            }
+            None => {
+                // Create only if not exists.
+                if let Err(e) = std::fs::create_dir(&path) {
+                    return Err(PartitionError::CreateDirectory(path, e));
+                }
+            }
+        }
+
+        step();
+    }
+
+    Ok(())
 }
 
 /// Represents an error when [`extract_firmware_dump()`] fails.
@@ -284,6 +394,28 @@ enum FirmwareError {
 
     #[error("couldn't extract {0}")]
     ExtractItem(String, #[source] Box<dyn Error>),
+}
+
+/// Represents an error when [`extract_partition()`] fails.
+#[derive(Debug, Error)]
+enum PartitionError {
+    #[error("unexpected device {0}")]
+    UnexpectedDevice(String),
+
+    #[error("couldn't get partition item")]
+    NextItem(#[source] obfw::ps4::PartError),
+
+    #[error("unexpected file {0}")]
+    UnexpectedFile(String),
+
+    #[error("couldn't create {0}")]
+    CreateDirectory(PathBuf, #[source] std::io::Error),
+
+    #[error("couldn't create {0}")]
+    CreateFile(PathBuf, #[source] std::io::Error),
+
+    #[error("couldn't extract {0} to {1}")]
+    ExtractFile(String, PathBuf, #[source] std::io::Error),
 }
 
 /// Represents an error when [`run_setup()`] fails.
