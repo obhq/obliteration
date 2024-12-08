c3918a49b1141d3c4bf31be761a45f179af3ac51
Putta Khunchalee <ultimaweapon@outlook.com>
2024-12-08 15:11:14 +0700

    Implements read_data_root for macOS

--- gui/src/setup/macos.rs
+++ gui/src/setup/macos.rs
@@ -1,7 +1,28 @@
+use core_foundation::base::TCFType;
+use core_foundation::propertylist::CFPropertyList;
+use core_foundation::string::CFString;
+use core_foundation_sys::preferences::{
+    kCFPreferencesCurrentApplication, CFPreferencesCopyAppValue,
+};
 use thiserror::Error;
 
 pub fn read_data_root() -> Result<Option<String>, DataRootError> {
-    todo!()
+    // Read value.
+    let key = CFString::from_static_string("DataRoot");
+    let val = unsafe {
+        CFPreferencesCopyAppValue(key.as_concrete_TypeRef(), kCFPreferencesCurrentApplication)
+    };
+
+    if val.is_null() {
+        return Ok(None);
+    }
+
+    // Convert value.
+    let val = unsafe { CFPropertyList::wrap_under_create_rule(val) };
+
+    val.downcast_into::<CFString>()
+        .ok_or_else(|| DataRootError::InvalidPreferenceValue(key.to_string()))
+        .map(|v| Some(v.to_string()))
 }
 
 pub fn write_data_root(path: impl AsRef<str>) -> Result<(), DataRootError> {
@@ -10,4 +31,7 @@ pub fn write_data_root(path: impl AsRef<str>) -> Result<(), DataRootError> {
 
 /// Represents an error when read or write data root fails.
 #[derive(Debug, Error)]
-pub enum DataRootError {}
+pub enum DataRootError {
+    #[error("invalid value for preference {0}")]
+    InvalidPreferenceValue(String),
+}
