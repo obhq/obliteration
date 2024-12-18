064abd4bb7dd96dbb5f92e90f6a8d747b1509211
Putta Khunchalee <ultimaweapon@outlook.com>
2024-12-18 23:55:14 +0700

    Implements RuntimeWindow::update_cursor

--- gui/src/rt/window.rs
+++ gui/src/rt/window.rs
@@ -1,11 +1,12 @@
 use std::error::Error;
-use winit::dpi::PhysicalSize;
+use winit::dpi::{PhysicalPosition, PhysicalSize};
 
 /// Encapsulates winit window with window-specific logic.
 ///
 /// The event loop will exit immediately if any method return an error.
 pub trait RuntimeWindow {
     fn update_size(&self, v: PhysicalSize<u32>) -> Result<(), Box<dyn Error + Send + Sync>>;
+    fn update_cursor(&self, v: PhysicalPosition<f64>) -> Result<(), Box<dyn Error + Send + Sync>>;
     fn update_scale_factor(&self, v: f64) -> Result<(), Box<dyn Error + Send + Sync>>;
     fn redraw(&self) -> Result<(), Box<dyn Error + Send + Sync>>;
 }
