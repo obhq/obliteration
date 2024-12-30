64d68d66e424fef72cd3b366a3d612a2e825dd0f
Putta Khunchalee <ultimaweapon@outlook.com>
2024-12-30 18:22:25 +0700

    Implements vCPU exit handler

--- gui/src/log/mod.rs
+++ gui/src/log/mod.rs
@@ -3,7 +3,7 @@ use anstyle_parse::Parser;
 use obconf::ConsoleType;
 use std::fs::File;
 use std::io::{stderr, stdout, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 mod file;
 
@@ -11,16 +11,23 @@ mod file;
 pub struct LogWriter {
     file: LogFile,
     parser: Parser,
+    path: PathBuf,
 }
 
 impl LogWriter {
-    pub fn new(file: &Path) -> Result<Self, std::io::Error> {
-        let file = File::create(file)?;
+    pub fn new(file: impl Into<PathBuf>) -> Result<Self, std::io::Error> {
+        let path = file.into();
+        let file = File::create(&path)?;
 
         Ok(Self {
             file: LogFile::new(file),
             parser: Parser::default(),
+            path,
         })
+    }
+
+    pub fn path(&self) -> &Path {
+        &self.path
     }
 
     pub fn write(&mut self, ty: ConsoleType, msg: String) {
