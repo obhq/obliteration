d0f8d8bf0b28374ffbed364a846c3461db81804e
Putta Khunchalee <ultimaweapon@outlook.com>
2025-01-10 00:20:40 +0700

    Initializes keg_ctor

--- kernel/src/uma/keg.rs
+++ kernel/src/uma/keg.rs
@@ -1,2 +1,17 @@
 /// Implementation of `uma_keg` structure.
 pub struct UmaKeg {}
+
+impl UmaKeg {
+    /// See `keg_ctor` on the Orbis for a reference.
+    ///
+    /// # Context safety
+    /// This function does not require a CPU context on **stage 1** heap.
+    ///
+    /// # Reference offsets
+    /// | Version | Offset |
+    /// |---------|--------|
+    /// |PS4 11.00|0x13CF40|
+    pub(super) fn new(_: usize) -> Self {
+        todo!()
+    }
+}
