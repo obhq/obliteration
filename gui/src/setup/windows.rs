9334c58098ee3b484fee7edf0658d9a98801d9ad
Tomáš Procházka <thompson2001@email.cz>
2025-01-12 15:26:54 +0100

    implement write data root

--- gui/src/setup/windows.rs
+++ gui/src/setup/windows.rs
@@ -3,9 +3,11 @@ use thiserror::Error;
 use windows_sys::w;
 use windows_sys::Win32::Foundation::{ERROR_FILE_NOT_FOUND, ERROR_SUCCESS};
 use windows_sys::Win32::System::Registry::{
-    RegCloseKey, RegCreateKeyExW, RegQueryValueExW, HKEY, HKEY_CURRENT_USER, KEY_ALL_ACCESS,
-    REG_OPTION_NON_VOLATILE, REG_SZ,
+    RegCloseKey, RegCreateKeyExW, RegQueryValueExW, RegSetValueExW, HKEY, HKEY_CURRENT_USER,
+    KEY_ALL_ACCESS, REG_OPTION_NON_VOLATILE, REG_SZ,
 };
+
+const FQVN: &str = "HKEY_CURRENT_USER\\Software\\OBHQ\\Obliteration\\DataRoot";
 
 pub fn read_data_root() -> Result<Option<String>, DataRootError> {
     // Open our registry key.
@@ -32,7 +34,6 @@ pub fn read_data_root() -> Result<Option<String>, DataRootError> {
     }
 
     // Get data size.
-    let fqvn = "HKEY_CURRENT_USER\\Software\\OBHQ\\Obliteration\\DataRoot";
     let key = Key(key);
     let name = w!("DataRoot");
     let mut ty = 0;
@@ -44,9 +45,9 @@ pub fn read_data_root() -> Result<Option<String>, DataRootError> {
     } else if e != ERROR_SUCCESS {
         let e = std::io::Error::from_raw_os_error(e.try_into().unwrap());
 
-        return Err(DataRootError::QueryRegKey(fqvn, e));
+        return Err(DataRootError::QueryRegKey(FQVN, e));
     } else if ty != REG_SZ {
-        return Err(DataRootError::InvalidRegValue(fqvn));
+        return Err(DataRootError::InvalidRegValue(FQVN));
     }
 
     // Read data.
@@ -65,9 +66,9 @@ pub fn read_data_root() -> Result<Option<String>, DataRootError> {
     if e != ERROR_SUCCESS {
         let e = std::io::Error::from_raw_os_error(e.try_into().unwrap());
 
-        return Err(DataRootError::QueryRegKey(fqvn, e));
+        return Err(DataRootError::QueryRegKey(FQVN, e));
     } else if ty != REG_SZ {
-        return Err(DataRootError::InvalidRegValue(fqvn));
+        return Err(DataRootError::InvalidRegValue(FQVN));
     }
 
     // Remove null-terminators if any.
@@ -79,12 +80,61 @@ pub fn read_data_root() -> Result<Option<String>, DataRootError> {
 
     // Convert to Rust string.
     String::from_utf16(&buf)
-        .map_err(|_| DataRootError::InvalidRegValue(fqvn))
+        .map_err(|_| DataRootError::InvalidRegValue(FQVN))
         .map(Some)
 }
 
 pub fn write_data_root(path: impl AsRef<str>) -> Result<(), DataRootError> {
-    todo!()
+    // Open our registry key.
+    let mut key = null_mut();
+    let key_path = "HKEY_CURRENT_USER\\Software\\OBHQ\\Obliteration";
+    let e = unsafe {
+        RegCreateKeyExW(
+            HKEY_CURRENT_USER,
+            w!("Software\\OBHQ\\Obliteration"),
+            0,
+            null(),
+            REG_OPTION_NON_VOLATILE,
+            KEY_ALL_ACCESS,
+            null(),
+            &mut key,
+            null_mut(),
+        )
+    };
+
+    if e != ERROR_SUCCESS {
+        let e = std::io::Error::from_raw_os_error(e.try_into().unwrap());
+        return Err(DataRootError::CreateRegKey(key_path, e));
+    }
+
+    // RAII to auto-close key.
+    let key = Key(key);
+
+    // Prepare the path in UTF-16 with a terminating 0.
+    let wide_path = path
+        .as_ref()
+        .encode_utf16()
+        .chain(std::iter::once(0))
+        .collect::<Vec<u16>>();
+
+    // Write the value.
+    let e = unsafe {
+        RegSetValueExW(
+            key.0,
+            w!("DataRoot"),
+            0,
+            REG_SZ,
+            wide_path.as_ptr().cast(),
+            (wide_path.len() * 2) as u32,
+        )
+    };
+
+    if e != ERROR_SUCCESS {
+        let e = std::io::Error::from_raw_os_error(e.try_into().unwrap());
+        return Err(DataRootError::WriteRegKey(FQVN, e));
+    }
+
+    Ok(())
 }
 
 /// RAII struct to close `HKEY` when dropped.
@@ -104,6 +154,9 @@ pub enum DataRootError {
 
     #[error("couldn't read {0}")]
     QueryRegKey(&'static str, #[source] std::io::Error),
+
+    #[error("couldn't write {0}")]
+    WriteRegKey(&'static str, #[source] std::io::Error),
 
     #[error("{0} has invalid value")]
     InvalidRegValue(&'static str),
