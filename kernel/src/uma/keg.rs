b84b0f0e41f88178b1cd3f7d13681edf56f2e553
Putta Khunchalee <ultimaweapon@outlook.com>
2025-01-12 21:00:34 +0700

    Implements uk_flags for keg_small_init (#1242)

--- kernel/src/uma/keg.rs
+++ kernel/src/uma/keg.rs
@@ -45,8 +45,25 @@ impl UmaKeg {
             if (size.get() + (min.size() - off)) > (PAGE_SIZE.get() - min.size()) {
                 todo!()
             } else {
-                todo!()
+                // TODO: Set uk_ppera, uk_ipers and uk_rsize.
+                if !flags.has(UmaFlags::Internal) {
+                    flags |= UmaFlags::Offpage;
+
+                    if !flags.has(UmaFlags::VToSlab) {
+                        flags |= UmaFlags::Hash;
+                    }
+                }
             }
         }
+
+        if flags.has(UmaFlags::Offpage) {
+            if flags.has(UmaFlags::RefCnt) {
+                // TODO: Set uk_slabzone to slabrefzone.
+            } else {
+                // TODO: Set uk_slabzone to slabzone.
+            }
+        }
+
+        todo!()
     }
 }
