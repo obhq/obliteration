cc27cbb46e08fe1fbf22ce84e78e14497aefe191
Putta Khunchalee <ultimaweapon@outlook.com>
2025-01-27 23:15:39 +0700

    Implements open_file for Windows

--- gui/src/ui/windows/dialogs.rs
+++ gui/src/ui/windows/dialogs.rs
@@ -1,9 +1,20 @@
 use crate::ui::{DesktopWindow, FileType};
-use futures::channel::oneshot;
 use raw_window_handle::{HasWindowHandle, RawWindowHandle};
+use std::ffi::OsString;
 use std::future::Future;
 use std::num::NonZero;
+use std::os::windows::ffi::OsStringExt;
 use std::path::PathBuf;
+use windows::core::w;
+use windows::Win32::Foundation::HWND;
+use windows::Win32::System::Com::{
+    CoCreateInstance, CoInitializeEx, CoUninitialize, CLSCTX_ALL, COINIT_APARTMENTTHREADED,
+    COINIT_SPEED_OVER_MEMORY,
+};
+use windows::Win32::UI::Shell::Common::COMDLG_FILTERSPEC;
+use windows::Win32::UI::Shell::{
+    FileOpenDialog, IFileOpenDialog, FOS_NOCHANGEDIR, SIGDN_FILESYSPATH,
+};
 use windows_sys::Win32::System::Com::CoTaskMemFree;
 use windows_sys::Win32::UI::Controls::Dialogs::{GetOpenFileNameW, OPENFILENAMEW};
 use windows_sys::Win32::UI::Shell::{
@@ -15,24 +26,67 @@ pub async fn open_file<T: DesktopWindow>(
     title: impl AsRef<str>,
     ty: FileType,
 ) -> Option<PathBuf> {
-    todo!()
-}
-
-pub async fn open_dir<T: DesktopWindow>(parent: &T, title: impl AsRef<str>) -> Option<PathBuf> {
-    let hwnd = get_hwnd(parent);
-
-    let title_wide: Vec<u16> = title
+    let parent = get_hwnd(parent);
+    let title: Vec<u16> = title
         .as_ref()
         .encode_utf16()
         .chain(std::iter::once(0))
         .collect();
+    let browse = move || unsafe {
+        // Setup CLSID_FileOpenDialog.
+        let browser: FileOpenDialog = CoCreateInstance(&FileOpenDialog, None, CLSCTX_ALL).unwrap();
+        let mut opts = browser.GetOptions().unwrap();
+        let filter = match ty {
+            FileType::Firmware => COMDLG_FILTERSPEC {
+                pszName: w!("Firmware Dump"),
+                pszSpec: w!("*.obf"),
+            },
+        };
 
-    spawn_dialog(move || unsafe {
+        opts |= FOS_NOCHANGEDIR;
+
+        browser.SetFileTypes(&[filter]).unwrap();
+        browser.SetOptions(opts).unwrap();
+        browser.SetTitle(title.as_ptr()).unwrap();
+
+        // Show CLSID_FileOpenDialog.
+        let item = match browser.Show(HWND(parent.get() as _)) {
+            Ok(_) => browser.GetResult().unwrap(),
+            Err(_) => return None,
+        };
+
+        // Get file path.
+        let buf = item.GetDisplayName(SIGDN_FILESYSPATH).unwrap();
+        let path = {
+            let mut len = 0;
+
+            while *buf.add(len) != 0 {
+                len += 1;
+            }
+
+            OsString::from_wide(std::slice::from_raw_parts(buf, len))
+        };
+
+        CoTaskMemFree(buf as _);
+
+        Some(PathBuf::from(path))
+    };
+
+    spawn_modal(browse).await
+}
+
+pub async fn open_dir<T: DesktopWindow>(parent: &T, title: impl AsRef<str>) -> Option<PathBuf> {
+    let parent = get_hwnd(parent);
+    let title: Vec<u16> = title
+        .as_ref()
+        .encode_utf16()
+        .chain(std::iter::once(0))
+        .collect();
+    let browse = move || unsafe {
         let mut bi: BROWSEINFOW = std::mem::zeroed();
-        bi.hwndOwner = hwnd.get() as _;
 
-        bi.lpszTitle = title_wide.as_ptr();
-
+        bi.hwndOwner = parent.get() as _;
+        bi.lpszTitle = title.as_ptr();
         bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE;
 
         let pidl = SHBrowseForFolderW(&mut bi);
@@ -53,30 +107,32 @@ pub async fn open_dir<T: DesktopWindow>(parent: &T, title: impl AsRef<str>) -> O
         } else {
             None
         }
-    })
-    .await
+    };
+
+    spawn_modal(browse).await
 }
 
-fn get_hwnd<T: DesktopWindow>(parent: &T) -> NonZero<isize> {
-    let parent = parent.handle();
-    let parent = parent.window_handle().unwrap();
-    let RawWindowHandle::Win32(win) = parent.as_ref() else {
+fn get_hwnd<T: DesktopWindow>(win: &T) -> NonZero<isize> {
+    let win = win.handle();
+    let win = win.window_handle().unwrap();
+    let RawWindowHandle::Win32(win) = win.as_ref() else {
         unreachable!();
     };
 
     win.hwnd
 }
 
-fn spawn_dialog<F>(dialog_fn: F) -> impl Future<Output = Option<PathBuf>>
+async fn spawn_modal<R, F>(f: F) -> R
 where
-    F: Send + 'static + FnOnce() -> Option<PathBuf>,
+    F: FnOnce() -> R + Send + 'static,
 {
-    let (tx, rx) = oneshot::channel();
+    let (tx, rx) = futures::channel::oneshot::channel();
 
-    std::thread::spawn(move || {
-        let res = dialog_fn();
-        tx.send(res).unwrap();
+    std::thread::spawn(move || unsafe {
+        CoInitializeEx(None, COINIT_APARTMENTTHREADED | COINIT_SPEED_OVER_MEMORY).unwrap();
+        tx.send(f()).unwrap();
+        CoUninitialize();
     });
 
-    async move { rx.await.unwrap_or(None) }
+    rx.await.unwrap()
 }
