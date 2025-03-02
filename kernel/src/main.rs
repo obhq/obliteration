820e2db0d262d345972e633a8ae1147a768927b5
Putta Khunchalee <ultimaweapon@outlook.com>
2025-03-02 20:47:55 +0900

    Populates initial_memory_size (#1300)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -86,8 +86,12 @@ fn run() -> ! {
 /// |---------|--------|
 /// |PS4 11.00|0x39A390|
 fn init_vm() -> Arc<Uma> {
+    // Initialize VM.
     let vm = Vm::new();
 
+    info!("Initial memory size is {}.", vm.initial_memory_size());
+
+    // Initialize UMA.
     Uma::new(vm)
 }
 
--- kernel/src/vm/mod.rs
+++ kernel/src/vm/mod.rs
@@ -1,6 +1,7 @@
 pub use self::object::*;
 
 use self::stats::VmStats;
+use crate::config::{PAGE_MASK, PAGE_SIZE};
 use crate::context::current_thread;
 use crate::lock::GutexGroup;
 use crate::proc::Proc;
@@ -14,6 +15,7 @@ mod stats;
 
 /// Implementation of Virtual Memory system.
 pub struct Vm {
+    initial_memory_size: u64,
     stats: [VmStats; 3],
     pagers: [Weak<Proc>; 2], // pageproc
 }
@@ -26,10 +28,6 @@ impl Vm {
     /// |---------|--------|
     /// |PS4 11.00|0x029200|
     pub fn new() -> Arc<Self> {
-        // The Orbis invoke this in hammer_time but we do it here instead to keep it in the VM
-        // subsystem.
-        Self::load_memory_map();
-
         // Initializes stats. The Orbis initialize these data in vm_pageout function but it is
         // possible for data race so we do it here instead.
         let pageout_page_count = 0x10; // TODO: Figure out where this value come from.
@@ -53,11 +51,25 @@ impl Vm {
             },
         ];
 
+        // The Orbis invoke this in hammer_time but we do it here instead to keep it in the VM
+        // subsystem.
+        let mut vm = Self {
+            initial_memory_size: 0,
+            stats,
+            pagers: Default::default(),
+        };
+
+        vm.load_memory_map();
+
         // Spawn page daemons. The Orbis do this in a separated sysinit but we do it here instead to
         // keep it in the VM subsystem.
-        let pagers = Self::spawn_pagers();
+        vm.spawn_pagers();
 
-        Arc::new(Self { stats, pagers })
+        Arc::new(vm)
+    }
+
+    pub fn initial_memory_size(&self) -> u64 {
+        self.initial_memory_size
     }
 
     /// See `vm_page_alloc` on the Orbis for a reference.
@@ -98,7 +110,8 @@ impl Vm {
     /// | Version | Offset |
     /// |---------|--------|
     /// |PS4 11.00|0x25CF00|
-    fn load_memory_map() {
+    fn load_memory_map(&mut self) {
+        // TODO: Some of the logic around physmap does not make sense.
         let mut physmap = [0u64; 60];
         let mut i = 0usize;
         let map = match boot_env() {
@@ -106,60 +119,72 @@ impl Vm {
         };
 
         'top: for m in map {
+            // We only interested in RAM.
             match m.ty {
                 MapType::None => break,
-                MapType::Ram => {
-                    // TODO: This should be possible only when booting from BIOS.
-                    if m.len == 0 {
-                        break;
-                    }
-
-                    let mut insert_idx = i + 2;
-                    let mut j = 0usize;
-
-                    while j <= i {
-                        if m.base < physmap[j + 1] {
-                            insert_idx = j;
-
-                            if physmap[j] < m.base + m.len {
-                                warn!("Overlapping memory regions, ignoring second region.");
-                                continue 'top;
-                            }
-
-                            break;
-                        }
-
-                        j += 2;
-                    }
-
-                    if insert_idx <= i && m.base + m.len == physmap[insert_idx] {
-                        physmap[insert_idx] = m.base;
-                        continue;
-                    }
-
-                    if insert_idx > 0 && m.base == physmap[insert_idx - 1] {
-                        physmap[insert_idx - 1] = m.base + m.len;
-                        continue;
-                    }
-
-                    i += 2;
-
-                    if i == physmap.len() {
-                        warn!("Too many segments in the physical address map, giving up.");
-                        break;
-                    }
-
-                    // This loop does not make sense on the Orbis. It seems like if this loop once
-                    // entered it will never exit.
-                    #[allow(clippy::while_immutable_condition)]
-                    while insert_idx < i {
-                        todo!()
-                    }
-
-                    physmap[insert_idx] = m.base;
-                    physmap[insert_idx + 1] = m.base + m.len;
-                }
+                MapType::Ram => (),
             }
+
+            // TODO: This should be possible only when booting from BIOS.
+            if m.len == 0 {
+                break;
+            }
+
+            let mut insert_idx = i + 2;
+            let mut j = 0usize;
+
+            while j <= i {
+                if m.base < physmap[j + 1] {
+                    if physmap[j] < m.base + m.len {
+                        warn!("Overlapping memory regions, ignoring second region.");
+                        continue 'top;
+                    }
+
+                    insert_idx = j;
+                    break;
+                }
+
+                j += 2;
+            }
+
+            if insert_idx <= i && m.base + m.len == physmap[insert_idx] {
+                physmap[insert_idx] = m.base;
+                continue;
+            }
+
+            if insert_idx > 0 && m.base == physmap[insert_idx - 1] {
+                physmap[insert_idx - 1] = m.base + m.len;
+                continue;
+            }
+
+            i += 2;
+
+            if i == physmap.len() {
+                warn!("Too many segments in the physical address map, giving up.");
+                break;
+            }
+
+            // This loop does not make sense on the Orbis. It seems like if this loop once
+            // entered it will never exit.
+            #[allow(clippy::while_immutable_condition)]
+            while insert_idx < i {
+                todo!()
+            }
+
+            physmap[insert_idx] = m.base;
+            physmap[insert_idx + 1] = m.base + m.len;
+        }
+
+        // Get initial memory size.
+        let physmap = &physmap[..i];
+        let page_size = PAGE_SIZE.get().try_into().unwrap();
+        let page_mask = !u64::try_from(PAGE_MASK.get()).unwrap();
+
+        for e in physmap.chunks(2) {
+            let start = e[0].next_multiple_of(page_size);
+            let end = e[1] & page_mask;
+
+            self.initial_memory_size += end.saturating_sub(start);
         }
     }
 
@@ -169,7 +194,7 @@ impl Vm {
     /// | Version | Offset |
     /// |---------|--------|
     /// |PS4 11.00|0x3E0E40|
-    fn spawn_pagers() -> [Weak<Proc>; 2] {
+    fn spawn_pagers(&mut self) {
         todo!()
     }
 }
