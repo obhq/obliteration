27b71c7ae0201913950b790e70ebdd939692be75
Putta Khunchalee <ultimaweapon@outlook.com>
2025-03-30 15:20:00 +0700

    Implements getenv

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -16,14 +16,18 @@ pub enum BootEnv {
 #[repr(C)]
 #[derive(Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(default))]
 pub struct Config {
     pub max_cpu: NonZero<usize>,
+    #[cfg_attr(feature = "serde", serde(with = "serde_bytes"))]
+    pub env_vars: [u8; 132096], // See init_dynamic_kenv() on the Orbis for this number.
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
             max_cpu: NonZero::new(1).unwrap(),
+            env_vars: [0; 132096],
         }
     }
 }
--- kernel/src/config/mod.rs
+++ kernel/src/config/mod.rs
@@ -1,7 +1,10 @@
 pub use self::arch::*;
 
+use alloc::boxed::Box;
 use alloc::sync::Arc;
+use alloc::vec::Vec;
 use core::num::NonZero;
+use krt::warn;
 use macros::elf_note;
 
 #[cfg_attr(target_arch = "aarch64", path = "aarch64.rs")]
@@ -14,12 +17,16 @@ pub const PAGE_MASK: NonZero<usize> = NonZero::new(PAGE_SIZE.get() - 1).unwrap()
 /// Runtime configurations for the kernel populated from [`config::Config`].
 pub struct Config {
     max_cpu: NonZero<usize>,
+    env_vars: Box<[&'static str]>, // kenvp
 }
 
 impl Config {
     pub fn new(src: &'static ::config::Config) -> Arc<Self> {
+        let env_vars = Self::load_env(src);
+
         Arc::new(Self {
             max_cpu: src.max_cpu,
+            env_vars,
         })
     }
 
@@ -27,8 +34,85 @@ impl Config {
         self.max_cpu
     }
 
-    pub fn env(&self, _: &str) -> Option<&'static str> {
-        todo!()
+    /// See `getenv` on the Orbis for a reference.
+    ///
+    /// # Reference offsets
+    /// | Version | Offset |
+    /// |---------|--------|
+    /// |PS4 11.00|0x39D0A0|
+    pub fn env(&self, name: &str) -> Option<&'static str> {
+        for &v in &self.env_vars {
+            // Check prefix.
+            let v = match v.strip_prefix(name) {
+                Some(v) => v,
+                None => continue,
+            };
+
+            // Check if '=' follow the name.
+            let mut iter = v.chars();
+
+            if iter.next().is_some_and(|c| c == '=') {
+                return Some(iter.as_str());
+            }
+        }
+
+        None
+    }
+
+    /// See `init_dynamic_kenv` on the Orbis for a reference.
+    ///
+    /// # Reference offsets
+    /// | Version | Offset |
+    /// |---------|--------|
+    /// |PS4 11.00|0x39DC90|
+    fn load_env(config: &'static ::config::Config) -> Box<[&'static str]> {
+        // Our implementation a bit different here. On Orbis they required the last entry to be an
+        // empty string but we don't.
+        let mut list = Vec::with_capacity(0x1000);
+        let mut rem = config.env_vars.as_slice();
+        let mut n = -1;
+
+        while !rem.is_empty() {
+            // We don't use https://crates.io/crates/memchr because it is likely to be useless since
+            // we don't have access to SIMD instructions in the kernel.
+            let v = match rem.iter().position(|&b| b == 0) {
+                Some(i) => {
+                    let v = &rem[..i];
+                    rem = &rem[(i + 1)..];
+                    v
+                }
+                None => core::mem::replace(&mut rem, b""),
+            };
+
+            // On Orbis they allow the first entry to be an empty string but I don't think it is
+            // intended behavior.
+            if v.is_empty() {
+                break;
+            }
+
+            n += 1;
+
+            // We required string to be UTF-8 while the Orbis does not.
+            let v = match core::str::from_utf8(v) {
+                Ok(v) => v,
+                Err(_) => {
+                    warn!("Ignoring non-UTF-8 kenv string #{n}.");
+                    continue;
+                }
+            };
+
+            if (v.len() + 1) >= 259 {
+                warn!("Too long kenv string, ignoring {v}.");
+                continue;
+            } else if list.len() > 511 {
+                warn!("Too many kenv strings, ignoring {v}.");
+                continue;
+            }
+
+            list.push(v);
+        }
+
+        list.into_boxed_slice()
     }
 }
 
