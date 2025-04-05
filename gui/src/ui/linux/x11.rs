c0cd24e17bf11b2d98edd9b4302d80fb7381f04f
Tomáš Procházka <tomas.prochazka@apertia.cz>
2025-04-05 18:21:27 +0200

    Implements set_center for X11

--- gui/src/ui/linux/x11.rs
+++ gui/src/ui/linux/x11.rs
@@ -5,6 +5,73 @@ use raw_window_handle::{HasWindowHandle, RawWindowHandle};
 use std::num::NonZero;
 use xcb::XidNew;
 
+pub unsafe fn set_center(x11: &X11, target: &impl DesktopWindow) -> Result<(), PlatformError> {
+    let target = target.handle();
+    let target_handle = target.window_handle().unwrap();
+
+    match (x11, target_handle.as_raw()) {
+        (X11::Xlib(xlib), RawWindowHandle::Xlib(target)) => {
+            let display = xlib.display().as_ptr();
+
+            let screen = unsafe { x11::xlib::XDefaultScreen(display) };
+
+            let mut attr = unsafe { std::mem::zeroed() };
+
+            let ret = unsafe { x11::xlib::XGetWindowAttributes(display, target.window, &mut attr) };
+
+            if let Some(err) = NonZero::new(ret) {
+                return Err(PlatformError::XlibGetWindowAttributes(err));
+            }
+
+            let screen_width = unsafe { x11::xlib::XDisplayWidth(display, screen) };
+            let screen_height = unsafe { x11::xlib::XDisplayHeight(display, screen) };
+
+            let x = (screen_width - attr.width) / 2;
+            let y = (screen_height - attr.height) / 2;
+
+            let ret = unsafe { x11::xlib::XMoveWindow(display, target.window, x, y) };
+
+            if let Some(err) = NonZero::new(ret) {
+                return Err(PlatformError::XlibCenterWindow(err));
+            }
+        }
+        (X11::Xcb(xcb), RawWindowHandle::Xcb(target)) => {
+            let connection = xcb.connection();
+
+            let window = unsafe { xcb::x::Window::new(target.window.get()) };
+
+            let geometry_cookie = connection.send_request(&xcb::x::GetGeometry {
+                drawable: xcb::x::Drawable::Window(window),
+            });
+            let geometry = connection
+                .wait_for_reply(geometry_cookie)
+                .map_err(PlatformError::XcbGetGeometry)?;
+
+            let setup = connection.get_setup();
+            let screen = setup.roots().next().unwrap();
+
+            let screen_width = screen.width_in_pixels();
+            let screen_height = screen.height_in_pixels();
+
+            let x = (screen_width as i32 - geometry.width() as i32) / 2;
+            let y = (screen_height as i32 - geometry.height() as i32) / 2;
+
+            let value_list = [xcb::x::ConfigWindow::X(x), xcb::x::ConfigWindow::Y(y)];
+            let cookie = connection.send_request_checked(&xcb::x::ConfigureWindow {
+                window,
+                value_list: &value_list,
+            });
+
+            connection
+                .check_request(cookie)
+                .map_err(PlatformError::XcbCenterWindow)?;
+        }
+        _ => unreachable!(),
+    }
+
+    Ok(())
+}
+
 pub unsafe fn set_modal(
     x11: &X11,
     target: &impl DesktopWindow,
