4e36481a1fa12e6756bc7ee94f178b9a8aa71bb5
Putta Khunchalee <ultimaweapon@outlook.com>
2025-04-06 17:47:17 +0900

    Adds Config::idps (#1325)

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,10 +1,12 @@
 #![no_std]
 
+pub use self::env::*;
+pub use self::idps::*;
+
 use core::num::NonZero;
 
-pub use self::env::*;
-
 mod env;
+mod idps;
 
 /// Contains information about the boot environment.
 #[repr(C)]
@@ -19,6 +21,7 @@ pub enum BootEnv {
 #[cfg_attr(feature = "serde", serde(default))]
 pub struct Config {
     pub max_cpu: NonZero<usize>,
+    pub idps: ConsoleId,
     #[cfg_attr(feature = "serde", serde(with = "serde_bytes"))]
     pub env_vars: [u8; 132096], // See init_dynamic_kenv() on the Orbis for this number.
 }
@@ -27,6 +30,7 @@ impl Default for Config {
     fn default() -> Self {
         Self {
             max_cpu: NonZero::new(1).unwrap(),
+            idps: ConsoleId::default(),
             env_vars: [0; 132096],
         }
     }
