b8d66c67813f40dfd396d465e15881ab2e76ad6b
Putta Khunchalee <ultimaweapon@outlook.com>
2025-08-10 10:48:02 +0700

    Populates dump_avail

--- kernel/build.rs
+++ kernel/build.rs
@@ -2,13 +2,9 @@ fn main() {
     let target = std::env::var("TARGET").unwrap();
 
     match target.as_str() {
-        "aarch64-unknown-none-softfloat" => {
-            println!("cargo::rustc-link-arg-bins=--pie");
-            println!("cargo::rustc-link-arg-bins=-zmax-page-size=0x4000");
-        }
-        "x86_64-unknown-none" => {
-            println!("cargo::rustc-link-arg-bins=-zmax-page-size=0x1000");
-        }
-        _ => {}
+        "aarch64-unknown-none-softfloat" => println!("cargo::rustc-link-arg-bins=--pie"),
+        _ => (),
     }
+
+    println!("cargo::rustc-link-arg-bins=-zmax-page-size=0x4000");
 }
