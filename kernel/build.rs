00775eebde19e25a1b608bd22f9ea3e2b7a076f7
Putta Khunchalee <ultimaweapon@outlook.com>
2025-08-10 11:04:56 +0700

    Fixes build.rs

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,10 +1,16 @@
 fn main() {
+    // Enable PIE on target that we need to build std. We need to check a triple here because this
+    // configuration is per-target.
     let target = std::env::var("TARGET").unwrap();
 
-    match target.as_str() {
-        "aarch64-unknown-none-softfloat" => println!("cargo::rustc-link-arg-bins=--pie"),
-        _ => (),
+    if target == "aarch64-unknown-none-softfloat" {
+        println!("cargo::rustc-link-arg-bins=--pie");
     }
 
-    println!("cargo::rustc-link-arg-bins=-zmax-page-size=0x4000");
+    // Set max-page-size.
+    let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    if os == "none" {
+        println!("cargo::rustc-link-arg-bins=-zmax-page-size=0x4000");
+    }
 }
