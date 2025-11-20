f5064437ae1dfcdedb0083733a44a378e23999cf
tompro <tomas.prochazka@apertia.cz>
2025-11-21 00:55:33 +0100

    gdb: Implement ?, qC, qfThreadInfo, qRegisterInfo and qsThreadInfo

--- gui/src/gdb/state.rs
+++ gui/src/gdb/state.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 /// Contains states for a GDB remote session.
 #[derive(Default)]
 pub struct SessionState {
@@ -109,5 +111,119 @@ impl SessionState {
     pub fn parse_vcont(&mut self, res: &mut Vec<u8>) {
         // Only Continue and Stop is supported at the moment.
         res.extend_from_slice(b"vCont;c;t");
+    }
+
+    pub fn parse_current_thread(&mut self, res: &mut Vec<u8>) {
+        // TODO: Retrieve actual thread ID from VMM instead of hardcoding.
+        // Return thread ID 1 as the current thread.
+        res.extend_from_slice(b"QC1");
+    }
+
+    pub fn parse_stop_reason(&mut self, res: &mut Vec<u8>) {
+        // Report stopped due to SIGTRAP (signal 5).
+        // Signal numbers are from the host OS. SIGTRAP is typically 5 on Unix systems.
+        // https://github.com/bminor/binutils-gdb/blob/83bf56647ce42ed79e5f007015afdd1f7a842d36/include/gdb/signals.def#L27
+        res.extend_from_slice(b"S05");
+    }
+
+    pub fn parse_first_thread_info(&mut self, res: &mut Vec<u8>) {
+        // TODO: Retrieve actual thread information from VMM instead of hardcoding.
+        // Return thread ID 1 as the only thread.
+        res.extend_from_slice(b"m1");
+    }
+
+    pub fn parse_subsequent_thread_info(&mut self, res: &mut Vec<u8>) {
+        // No more threads.
+        res.extend_from_slice(b"l");
+    }
+
+    pub fn parse_register_info(&mut self, reg: &[u8], res: &mut Vec<u8>) {
+        // Parse register number.
+        let reg = match std::str::from_utf8(reg)
+            .ok()
+            .and_then(|s| usize::from_str_radix(s, 16).ok())
+        {
+            Some(v) => v,
+            None => return,
+        };
+
+        // Get register info based on architecture.
+        if cfg!(target_arch = "aarch64") {
+            self.parse_aarch64_register_info(reg, res);
+        } else if cfg!(target_arch = "x86_64") {
+            self.parse_x86_64_register_info(reg, res);
+        } else {
+            todo!()
+        }
+    }
+
+    fn parse_aarch64_register_info(&mut self, reg: usize, res: &mut Vec<u8>) {
+        // Register numbering follows the DWARF for ARM 64-bit Architecture (AArch64) specification.
+        // See https://github.com/ARM-software/abi-aa/blob/main/aadwarf64/aadwarf64.rst
+        let info = match reg {
+            0..=28 => Cow::Owned(format!(
+                "name:x{reg};bitsize:64;offset:{};encoding:uint;format:hex;set:General Purpose Registers;gcc:{reg};dwarf:{reg};",
+                reg * 8,
+            )),
+            29 => Cow::Borrowed(
+                "name:fp;bitsize:64;offset:232;encoding:uint;format:hex;set:General Purpose Registers;gcc:29;dwarf:29;",
+            ),
+            30 => Cow::Borrowed(
+                "name:lr;bitsize:64;offset:240;encoding:uint;format:hex;set:General Purpose Registers;gcc:30;dwarf:30;",
+            ),
+            31 => Cow::Borrowed(
+                "name:sp;bitsize:64;offset:248;encoding:uint;format:hex;set:General Purpose Registers;gcc:31;dwarf:31;generic:sp;",
+            ),
+            32 => Cow::Borrowed(
+                "name:pc;bitsize:64;offset:256;encoding:uint;format:hex;set:General Purpose Registers;gcc:32;dwarf:32;generic:pc;",
+            ),
+            33 => Cow::Borrowed(
+                "name:cpsr;bitsize:32;offset:264;encoding:uint;format:hex;set:General Purpose Registers;",
+            ),
+            _ => return, // No more registers.
+        };
+
+        res.extend_from_slice(info.as_bytes());
+    }
+
+    fn parse_x86_64_register_info(&mut self, reg: usize, res: &mut Vec<u8>) {
+        // (name, gcc/dwarf number, generic)
+        // See https://gitlab.com/x86-psABIs/x86-64-ABI (Figure 3.36: DWARF Register Number Mapping)
+        const REGS: &[(&str, usize, &str)] = &[
+            ("rax", 0, ""),
+            ("rbx", 3, ""),
+            ("rcx", 2, ""),
+            ("rdx", 1, ""),
+            ("rsi", 4, ""),
+            ("rdi", 5, ""),
+            ("rbp", 6, "generic:fp;"),
+            ("rsp", 7, "generic:sp;"),
+            ("r8", 8, ""),
+            ("r9", 9, ""),
+            ("r10", 10, ""),
+            ("r11", 11, ""),
+            ("r12", 12, ""),
+            ("r13", 13, ""),
+            ("r14", 14, ""),
+            ("r15", 15, ""),
+            ("rip", 16, "generic:pc;"),
+        ];
+
+        let info = match reg {
+            0..=16 => {
+                let (name, dwarf, generic) = REGS[reg];
+
+                Cow::Owned(format!(
+                    "name:{name};bitsize:64;offset:{};encoding:uint;format:hex;set:General Purpose Registers;gcc:{dwarf};dwarf:{dwarf};{generic}",
+                    reg * 8,
+                ))
+            }
+            17 => Cow::Borrowed(
+                "name:rflags;bitsize:32;offset:136;encoding:uint;format:hex;set:General Purpose Registers;",
+            ),
+            _ => return, // No more registers.
+        };
+
+        res.extend_from_slice(info.as_bytes());
     }
 }
