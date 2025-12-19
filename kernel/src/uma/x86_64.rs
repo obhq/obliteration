66194ae1247906a4e94031ca89aa704849ff1abc
Putta Khunchalee <ultimaweapon@outlook.com>
2025-12-20 06:22:18 +0700

    Finishes minimal implementation of vm_page_alloc (#1418)

--- kernel/src/uma/x86_64.rs
+++ kernel/src/uma/x86_64.rs
@@ -1,5 +1,6 @@
 use super::Alloc;
 use crate::vm::Vm;
+use core::sync::atomic::{AtomicUsize, Ordering};
 
 /// See `uma_small_alloc` on the Orbis for a reference.
 ///
@@ -8,9 +9,13 @@ use crate::vm::Vm;
 /// |---------|--------|
 /// |PS4 11.00|0x22FD70|
 pub fn small_alloc(vm: &Vm, flags: Alloc) {
-    // TODO: There are an increment on an unknown variable on the Orbis.
+    // TODO: Figure out the name of this static variable. Also the Orbis does not use atomic
+    // operation here.
+    static UNK: AtomicUsize = AtomicUsize::new(0);
+
     vm.alloc_page(
         None,
+        UNK.fetch_add(1, Ordering::Relaxed),
         // TODO: Refactor this for readability.
         ((((u32::from(flags) & 0x100) >> 2) - (u32::from((u32::from(flags) & 0x401) == 1)) + 0x22)
             | 0x100)
