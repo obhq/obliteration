aafb06e6e854e6e0f7c6829a36af4c798537a0d8
Putta Khunchalee <ultimaweapon@outlook.com>
2026-02-08 16:38:39 +0700

    Populates uma_slab::us_freelist in keg_alloc_slab

--- kernel/src/uma/slab.rs
+++ kernel/src/uma/slab.rs
@@ -3,13 +3,18 @@
 /// We use slightly different mechanism here but has the same memory layout.
 #[repr(C)]
 pub struct Slab<I: ?Sized> {
-    free: I, // us_freelist
+    pub free: I, // us_freelist
 }
 
 /// Item in the slab to represents `uma_slab` structure.
 #[repr(C)]
-pub struct Free {}
+pub struct Free {
+    pub item: u8, // us_item
+}
 
 /// Item in the slab to represents `uma_slab_refcnt` structure.
 #[repr(C)]
-pub struct RcFree {}
+pub struct RcFree {
+    pub item: u8,    // us_item
+    pub refcnt: u32, // us_refcnt
+}
