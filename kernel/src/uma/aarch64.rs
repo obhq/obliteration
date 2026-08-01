c2461193eaf8d10a0006629f974431e4b6b90686
Putta Khunchalee <ultimaweapon@outlook.com>
2026-08-01 14:14:59 +0700

    Fixes error on AArch64

--- kernel/src/uma/aarch64.rs
+++ kernel/src/uma/aarch64.rs
@@ -1,6 +1,6 @@
-use super::Alloc;
+use super::{Alloc, SlabFlags};
 use crate::vm::Vm;
 
-pub fn small_alloc(vm: &'static Vm, flags: Alloc) -> *mut u8 {
+pub fn small_alloc(vm: &'static Vm, flags: Alloc) -> (*mut u8, SlabFlags) {
     todo!()
 }
